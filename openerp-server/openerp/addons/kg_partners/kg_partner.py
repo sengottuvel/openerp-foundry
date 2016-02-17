59b831e5e1f3734162a9288164581f236cbca9c7
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-02-17 13:07:33 +0530

    Supplier Master Changes

--- openerp-server/openerp/addons/kg_partners/kg_partner.py
+++ openerp-server/openerp/addons/kg_partners/kg_partner.py
@@ -2,8 +2,8 @@ from functools import partial
 import logging
 from lxml import etree
 from lxml.builder import E
-
 import openerp
+import time
 from openerp import SUPERUSER_ID
 from openerp import pooler, tools
 import openerp.exceptions
@@ -21,7 +21,125 @@ class kg_partner(osv.osv):
 	_columns = {
 	
 	'city_id' : fields.many2one('res.city', 'City'),
-		
+	'tin_no' : fields.char('TIN'),
+	'pan_no' : fields.char('PAN'),
+	'tan_no' : fields.char('TAN'),
+	'cst_no' : fields.char('CST'),
+	'gst_no' : fields.char('GST'),
+	'supply_type': fields.selection([('material','Material'),('service','Service'),('contractor','Contractor'),('labour','Labour'),('all','All')],'Supply Type'),
+	'company_type': fields.selection([('individual','Individual'),('company','Company'),('trust','Trust')],'Type'),
+	'tds': fields.selection([('yes','Yes'),('no','No')],'TDS Applicable'),
+	'grade': fields.selection([('a','A'),('b','B'),('c','C')],'Grade'),
+	'payment_id': fields.many2one('kg.payment.master','Payment Terms'),
+	'language': fields.selection([('tamil', 'Tamil'),('english', 'English'),('hindi', 'Hindi'),('malayalam', 'Malayalam'),('others','Others')],'Preferred Language'),
+	'cheque_in_favour': fields.char('Cheque in Favor Of'),
+	'advance_limit': fields.char('Advance Limit'),
+	'transport': fields.char('Transport'),
+	'contact_person': fields.char('Contact Person', size=128),
+	'landmark': fields.char('Landmark', size=128),
+	'partner_state': fields.selection([('draft','Draft'),('approve','Approved')],'Status'),
+	'group_flag': fields.boolean('Is Group Company'),
+	'delivery_id': fields.many2one('kg.delivery.master','Delivery Type'),
+	#'child_ids': fields.one2many('res.partner', 'parent_id', 'Contacts', domain=[('active','=',True)]),
+	'creation_date': fields.datetime('Creation Date',readonly=True),
+	'created_by': fields.many2one('res.users', 'Created by',readonly=True),
+	'confirmed_date': fields.datetime('Confirmed Date',readonly=True),
+	'confirmed_by': fields.many2one('res.users','Confirmed By',readonly=True),
+	'approved_date': fields.datetime('Approved Date',readonly=True),
+	'approved_by': fields.many2one('res.users','Approved By',readonly=True),
+	'updated_date': fields.datetime('Last Update Date',readonly=True),
+	'updated_by': fields.many2one('res.users','Last Updated By',readonly=True),
+	
+	'con_designation': fields.char('Designation'),
+	'con_whatsapp': fields.char('Whatsapp No'),
+	'acc_number': fields.char('Whatsapp No'),
+	'bank_name': fields.char('Whatsapp No'),
+	'bank_bic': fields.char('Whatsapp No'),
+	
+	'delivery_ids':fields.one2many('kg.delivery.address', 'src_id', 'Delivery Address'),
+	
+	'billing_ids':fields.one2many('kg.billing.address', 'bill_id', 'Billing Address'),
+	
+	
 	}
+	
+	_defaults = {
+	  
+	  'is_company': True,
+	  'creation_date': fields.datetime.now,
+	  'created_by': lambda obj, cr, uid, context: uid,
+	 
+	}
+	
+	
+	def approve_partner(self, cr, uid, ids, context=None): 
+		rec = self.browse(cr, uid, ids[0])
+		self.write(cr, uid, ids, {'partner_state': 'approve','approved_by':uid,'approved_date': time.strftime('%Y-%m-%d %H:%M:%S')})
 		
+		return True
+	
+	def write(self, cr, uid, ids, vals, context=None):		
+		vals.update({'updated_date': time.strftime('%Y-%m-%d %H:%M:%S'),'updated_by':uid})
+		return super(kg_partner, self).write(cr, uid, ids, vals, context)
+			
 kg_partner()
+
+
+
+class kg_delivery_address(osv.osv):
+
+	_name = "kg.delivery.address"
+	_description = "Delivery Address"
+	
+	_columns = {
+
+	'src_id': fields.many2one('res.partner', 'Partner Master'),
+	'street': fields.char('Street', size=128,select=True),
+	'street1': fields.char('Street 1', size=128,select=True),
+	'landmark': fields.char('Landmark',size=128),
+	'city_id': fields.many2one('res.city', 'City',select=True),
+	'state_id': fields.many2one('res.country.state', 'State'),
+	'country_id': fields.many2one('res.country', 'Country'),	
+	'contact_no': fields.char('Contact No', size=128),
+	'zip': fields.char('ZIP', size=128),
+	'date': fields.date('Creation Date'),
+	'default': fields.boolean('Default'),
+	
+	}
+
+	_defaults = {
+
+	'date' : fields.date.context_today,
+
+	} 
+
+kg_delivery_address()
+
+class kg_billing_address(osv.osv):
+
+	_name = "kg.billing.address"
+	_description = "Billing Address"
+	
+	_columns = {
+
+	'bill_id': fields.many2one('res.partner', 'Partner Master'),
+	'street': fields.char('Street', size=128,select=True),
+	'street1': fields.char('Street 1', size=128,select=True),
+	'landmark': fields.char('Landmark',size=128),
+	'city_id': fields.many2one('res.city', 'City',select=True),
+	'state_id': fields.many2one('res.country.state', 'State'),
+	'country_id': fields.many2one('res.country', 'Country'),	
+	'contact_no': fields.char('Contact No', size=128),
+	'zip': fields.char('ZIP', size=128),
+	'date': fields.date('Creation Date'),
+	'default': fields.boolean('Default'),
+	
+	}
+
+	_defaults = {
+
+	'date' : fields.date.context_today,
+
+	} 
+
+kg_billing_address()
