6ebf474d9b89c559ef3107f9599d72d5677e700c
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-03-08 11:23:48 +0530

    Set to Draft and Day by day delivery Karthikeyan Changes

--- openerp-server/openerp/addons/kg_menus/__openerp__.py
+++ openerp-server/openerp/addons/kg_menus/__openerp__.py
@@ -28,8 +28,8 @@ This module allows you to manage your Project Menus
 """,
     'depends' : ['base', 'mrp', 'stock','kg_pumpmodel_master','kg_reports'],
     'data': [
-				'kg_groups.xml',
-				'kg_menus_view.xml',
+				#'kg_groups.xml',
+				#'kg_menus_view.xml',
 			],
     'auto_install': False,
     'installable': True,
