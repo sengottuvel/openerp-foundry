f6fe5d66ba5406c9feb28962b9068aed3b43f4b7
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-04-07 11:37:04 +0530

    Moc Construction Type Added By Karthikeyan

---
+++ openerp-server/openerp/addons/kg_construction_type/__openerp__.py
@@ -0,0 +1,18 @@
+##############################################################################
+#
+#   This is New Customized Pump Model Master
+#
+##############################################################################
+
+{
+    'name': 'Construction Type Master',
+    'version': '0.1',
+    'author': 'Karthikeyan',
+    'category': 'BASE',        
+    'depends' : ['base'],
+    'data': ['kg_construction_type_view.xml'],
+    'css': ['static/src/css/state.css'], 
+    'auto_install': False,
+    'installable': True,
+}
+
--- openerp-server/openerp/addons/kg_construction_type/kg_construction_type.py
+++ openerp-server/openerp/addons/kg_construction_type/kg_construction_type.py
@@ -8,34 +8,35 @@ import re
 import math
 dt_time = time.strftime('%m/%d/%Y %H:%M:%S')
 
-class kg_moc_construction(osv.osv):
+class kg_construction_type(osv.osv):
 
-	_name = "kg.moc.construction"
-	_description = "MOC Construction Master"
+	_name = "kg.construction.type"
+	_description = "Construction Type Master"
 	
+	"""
 	def _get_modify(self, cr, uid, ids, field_name, arg, context=None):
 		res={}
-		ch_weekly_obj = self.pool.get('ch.weekly.schedule.details')			
+		stock_obj = self.pool.get('kg.stock.inward')
+		weekly_sch_obj = self.pool.get('kg.weekly.schedule')				
 		for item in self.browse(cr, uid, ids, context=None):
 			res[item.id] = 'no'
-			ch_weekly_ids = ch_weekly_obj.search(cr,uid,[('moc_construction_id','=',item.id)])			
-			if ch_weekly_ids:
+			stock_ids = stock_obj.search(cr,uid,[('division_id','=',item.id)])
+			weekly_sch_ids = weekly_sch_obj.search(cr,uid,[('division_id','=',item.id)])			
+			if stock_ids or weekly_sch_ids:
 				res[item.id] = 'yes'		
 		return res
-	
+	"""
 	_columns = {
 			
 		'name': fields.char('Name', size=128, required=True, select=True),
 		'company_id': fields.many2one('res.company', 'Company Name',readonly=True),
-		'code': fields.char('Code', size=128, required=True),		
+		'code': fields.char('Code', size=128, required=True),
 		'active': fields.boolean('Active'),
 		'state': fields.selection([('draft','Draft'),('confirmed','WFA'),('approved','Approved'),('reject','Rejected'),('cancel','Cancelled')],'Status', readonly=True),
 		'notes': fields.text('Notes'),
 		'remark': fields.text('Approve/Reject'),
-		'cancel_remark': fields.text('Cancel'),			
-		
-		'modify': fields.function(_get_modify, string='Modify', method=True, type='char', size=10),		
-		'constuction_type_id': fields.many2one('kg.construction.type','Construction Type', required=True,domain="[('active','=','t')]"),
+		'cancel_remark': fields.text('Cancel'),
+		#'modify': fields.function(_get_modify, string='Modify', method=True, type='char', size=10),		
 		
 		### Entry Info ###
 		'crt_date': fields.datetime('Creation Date',readonly=True),
@@ -53,43 +54,29 @@ class kg_moc_construction(osv.osv):
 	
 	_defaults = {
 	
-		'company_id': lambda self,cr,uid,c: self.pool.get('res.company')._company_default_get(cr, uid, 'kg.moc.construction', context=c),
+		'company_id': lambda self,cr,uid,c: self.pool.get('res.company')._company_default_get(cr, uid, 'kg.construction.type', context=c),
 		'active': True,
 		'state': 'draft',
 		'user_id': lambda obj, cr, uid, context: uid,
 		'crt_date':fields.datetime.now,	
-		'modify': 'no',
+		#'modify': 'no',
 		
 	}
 	
 	_sql_constraints = [
 	
-		('name', 'unique(name)', 'Name must be unique!!'),
-		('code', 'unique(code)', 'Code must be unique!!'),
+		('name', 'unique(name)', 'Name must be unique per Company !!'),
+		('code', 'unique(code)', 'Code must be unique per Company !!'),
 	]
 	
-	"""def _Validation(self, cr, uid, ids, context=None):
-		flds = self.browse(cr , uid , ids[0])
-		name_special_char = ''.join( c for c in flds.name if  c in '!@#$%^~*{}?+/=' )		
-		if name_special_char:
-			return False		
-		return True	
-		
-	def _CodeValidation(self, cr, uid, ids, context=None):
-		flds = self.browse(cr , uid , ids[0])	
-		if flds.code:		
-			code_special_char = ''.join( c for c in flds.code if  c in '!@#$%^~*{}?+/=' )		
-			if code_special_char:
-				return False
-		return True		"""
 		
 	def _name_validate(self, cr, uid,ids, context=None):
 		rec = self.browse(cr,uid,ids[0])
 		res = True
 		if rec.name:
-			moc_name = rec.name
-			name=moc_name.upper()			
-			cr.execute(""" select upper(name) from kg_moc_construction where upper(name)  = '%s' """ %(name))
+			const_type_name = rec.name
+			name=const_type_name.upper()			
+			cr.execute(""" select upper(name) from kg_construction_type where upper(name)  = '%s' """ %(name))
 			data = cr.dictfetchall()			
 			if len(data) > 1:
 				res = False
@@ -101,9 +88,9 @@ class kg_moc_construction(osv.osv):
 		rec = self.browse(cr,uid,ids[0])
 		res = True
 		if rec.code:
-			moc_code = rec.code
-			code=moc_code.upper()			
-			cr.execute(""" select upper(code) from kg_moc_construction where upper(code)  = '%s' """ %(code))
+			const_type_code = rec.code
+			code=const_type_code.upper()			
+			cr.execute(""" select upper(code) from kg_construction_type where upper(code)  = '%s' """ %(code))
 			data = cr.dictfetchall()			
 			if len(data) > 1:
 				res = False
@@ -121,10 +108,8 @@ class kg_moc_construction(osv.osv):
 		return True
 
 	def entry_confirm(self,cr,uid,ids,context=None):
-		rec = self.browse(cr,uid,ids[0])		
 		self.write(cr, uid, ids, {'state': 'confirmed','confirm_user_id': uid, 'confirm_date': time.strftime('%Y-%m-%d %H:%M:%S')})
 		return True
-		
 	def entry_draft(self,cr,uid,ids,context=None):
 		self.write(cr, uid, ids, {'state': 'draft'})
 		return True
@@ -154,17 +139,13 @@ class kg_moc_construction(osv.osv):
 		
 	def write(self, cr, uid, ids, vals, context=None):
 		vals.update({'update_date': time.strftime('%Y-%m-%d %H:%M:%S'),'update_user_id':uid})
-		return super(kg_moc_construction, self).write(cr, uid, ids, vals, context)
+		return super(kg_construction_type, self).write(cr, uid, ids, vals, context)
 		
 	
-	_constraints = [
-		#(_Validation, 'Special Character Not Allowed !!!', ['Check Name']),
-		#(_CodeValidation, 'Special Character Not Allowed !!!', ['Check Code']),
-		(_name_validate, 'MOC Construction name must be unique !!', ['name']),		
-		(_code_validate, 'MOC Construction code must be unique !!', ['code']),		
+	_constraints = [		
+		(_name_validate, 'Construction Type name must be unique !!', ['name']),		
+		(_code_validate, 'Construction Type code must be unique !!', ['code']),		
 		
 	]
 	
-kg_moc_construction()
-
-
+kg_construction_type()
