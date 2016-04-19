afbc917ac6b97121e969b3057b43feb3187e02cf
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-04-19 19:38:23 +0530

    All PPC changes 19-04-2016 By Karthikeyan

---
+++ openerp-server/openerp/addons/kg_users_rules/__openerp__.py
@@ -0,0 +1,30 @@
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+
+##############################################################################
+{
+    'name': 'KG Vendor Quote Users',
+    'version': '0.1',
+    'author': 'Ramya Kalaiselvan',
+    'category': 'User_Management',
+    'images': ['images/purchase_requisitions.jpeg'],
+    'website': 'http://www.openerp.com',
+    'description': """
+This module allows you to manage your Purchase Requisition.
+===========================================================
+
+When a purchase order is created, you now have the opportunity to save the
+related requisition. This new object will regroup and will allow you to easily
+keep track and order all your purchase orders.
+""",
+    'depends' : ['base','kg_groups','kg_quotation'],
+    'data': [
+			'kg_model_rules.xml',
+			],
+			
+    'auto_install': False,
+    'installable': True,
+}
+
