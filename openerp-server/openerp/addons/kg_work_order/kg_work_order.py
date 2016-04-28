7c21a30e7f2c6ab3d5dd4e4f6e7ef4888ba91967
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-04-28 13:12:35 +0530

    PPC Transactions by Sangeetha

--- openerp-server/openerp/addons/kg_work_order/kg_work_order.py
+++ openerp-server/openerp/addons/kg_work_order/kg_work_order.py
@@ -363,7 +363,7 @@ class kg_work_order(osv.osv):
 		encoded_user = base64.b64encode(rec_user)
 		encoded_pwd = base64.b64encode(rec_pwd)
 		
-		url = 'http://10.100.9.60/DMS/login.html?xmxyypzr='+encoded_user+'&mxxrqx='+encoded_pwd+'&wo_no='+rec_work_order
+		url = 'http://192.168.1.7/DMS/login.html?xmxyypzr='+encoded_user+'&mxxrqx='+encoded_pwd+'&wo_no='+rec_work_order
 		
 		#url = 'http://192.168.1.150:81/pbxclick2call.php?exten='+exe_no+'&phone='+str(m_no)
 		return {
