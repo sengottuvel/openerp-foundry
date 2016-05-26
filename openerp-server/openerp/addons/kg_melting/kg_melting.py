8349368348ec622db86b69ab060737bea0b85421
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-05-26 16:23:36 +0530

    PO COPY and Melting log

--- openerp-server/openerp/addons/kg_melting/kg_melting.py
+++ openerp-server/openerp/addons/kg_melting/kg_melting.py
@@ -15,6 +15,31 @@ class kg_melting(osv.osv):
 	_description = "Melting Log"
 	_order = "entry_date desc"
 	
+	def _get_various_amt(self, cr, uid, ids, field_name, arg, context=None):
+		result = {}
+		melt_cost = 0
+		grand_total = 0
+		various_formula = 0
+		for entry in self.browse(cr, uid, ids, context=context):
+			for line in entry.line_ids:				
+				melt_cost += line.total_amount			
+				grand_total += line.total_weight	
+				
+			various_formula = ((entry.total_weight_metal/grand_total)/grand_total)	* 100
+			result[entry.id] = various_formula
+		return result
+		
+	def _get_melting_cost(self, cr, uid, ids, field_name, arg, context=None):
+		result = {}
+		melt_cost = 0		
+		melting_total_rate = 0
+		for entry in self.browse(cr, uid, ids, context=context):
+			for line in entry.line_ids:			
+				melt_cost += line.total_amount					
+			melting_total_rate = melt_cost + entry.amount
+			result[entry.id] = melting_total_rate
+		return result
+	
 	_columns = {
 	
 		### Header Details ####
@@ -48,7 +73,7 @@ class kg_melting(osv.osv):
 		'initial_reading_type': fields.selection([('unit','Units'),('ton','Ton')],'Initial Reading Type', required=True),	
 		
 		'total_units': fields.float('Total Units',readonly=True),		
-		'amount': fields.float('Amount',required=True),		
+		'amount': fields.float('Amount',readonly=True),		
 		
 		'pouring_temp': fields.float('Pouring Temp',required=True),
 		'pouring_hrs': fields.float('Pouring Hours',required=True),
@@ -59,15 +84,20 @@ class kg_melting(osv.osv):
 		'pouring_finished_time_type': fields.selection([('am','AM'),('pm','PM')],'Time Type', required=True),
 		'liquid_metal_wt': fields.float('Liquid Metal Wt.',required=True,digits=(16,3)),	
 		'ingot_wt': fields.float('Ingot Wt.',required=True,digits=(16,3)),	
+		'total_weight_metal': fields.float('Total Weight',digits=(16,3)),	
+		
+		
+		'various': fields.function(_get_various_amt, string='Various(kg)',digits=(16,3), method=True, store=True, type='float'),		
+		'melt_cost': fields.function(_get_melting_cost, string='Melting Cost(Rs.)', method=True, store=True, type='float'),
 		
 		'load_item': fields.boolean('Load Item'),
 		
 		##### Worker Details ####
-		'supervisor_name': fields.many2one('res.partner','Supervisor Name', domain="[('active','=','t')]"),
-		'done_by': fields.selection([('company_employee','Company Employee'),('contractor','Contractor')],'Done By'),		
-		'employee_id': fields.many2many('res.partner', 'm2m_moc_employee_details', 'melting_emp_id', 'employee_id','Name', domain="[('active','=','t')]"),
-		'helper_count': fields.float('Helper Count'),	
-		'contractor_id': fields.many2one('res.partner','Contractor Name',domain="[('active','=','t')]"),
+		'supervisor_name': fields.many2one('res.partner','Supervisor Name', required=True,domain="[('active','=','t')]"),
+		'done_by': fields.selection([('company_employee','Company Employee'),('contractor','Contractor')],'Done By', required=True),		
+		'employee_id': fields.many2many('res.partner', 'm2m_moc_employee_details', 'melting_emp_id', 'employee_id','Name'),
+		'helper_count': fields.float('Helper Count',required=True),	
+		'contractor_id': fields.many2one('res.partner','Contractor Name'),
 		
 		
 		### Entry Info ####
@@ -107,6 +137,23 @@ class kg_melting(osv.osv):
     ]
 	
 	
+	### Added by Sangeetha ###
+	def onchange_reading(self,cr, uid, ids, initial_reading,final_reading, context=None):
+		if initial_reading > 0 and final_reading > 0:
+			total_reading = final_reading - initial_reading
+			total_value = total_reading * 6.25
+		else:
+			total_reading = 0.00
+			total_value = 0.00
+		return {'value': {'total_units': total_reading,'amount':total_value}}
+	### Ends Here ###
+	
+	def onchange_weight(self,cr, uid, ids, liquid_metal_wt,ingot_wt, context=None):
+		
+		total_weight = liquid_metal_wt + ingot_wt
+		
+		return {'value': {'total_weight_metal': total_weight}}
+	
 	
 	def load_item(self, cr, uid, ids, context=None):		
 		
@@ -128,27 +175,38 @@ class kg_melting(osv.osv):
 				raw_material_line_id = raw_material_line_obj.create(cr,uid,
 					{
 						'header_id':rec.id,
-						'product_id': raw_material_line.product_id.id,						
+						'product_id': raw_material_line.product_id.id,
+						'moc_id':rec.moc_id.id			
 					})
 			cr.execute(""" delete from ch_melting_chemistry_details where header_id  = %s """ %(ids[0]))	
 			for chemistry_details_line in chemistry_details_lines:					
 				chemistry_details_line_id = chemistry_details_line_obj.create(cr,uid,
 					{
 						'header_id':rec.id,
-						'chemistry_id': chemistry_details_line.chemical_id.id,						
+						'chemistry_id': chemistry_details_line.chemical_id.id,
+												
 					})	
 		self.write(cr, uid, ids, {'load_item': True})
 		return True
 	
 	def entry_confirm(self,cr,uid,ids,context=None):
-		entry = self.browse(cr,uid,ids[0])
-		power_details=entry.final_reading - entry.initial_reading
-		total_value=power_details*1000
-		for item in entry.line_ids:
-			total = item.first_addition + item.second_addition
-			print"total",total
-			self.pool.get('ch.melting.charge.details').write(cr, uid, item.id, {'total_weight': total})	
-		self.write(cr, uid, ids, {'state': 'confirmed','confirm_user_id': uid, 'confirm_date': time.strftime('%Y-%m-%d %H:%M:%S'),'total_units': total_value})
+		entry = self.browse(cr,uid,ids[0])		
+		line_ids =entry.line_ids
+		grand_total =0.00
+		melt_cost =0.00
+		for line in line_ids:			
+			melt_cost += line.total_amount			
+			grand_total += line.total_weight	
+				
+		various_formula = ((entry.total_weight_metal/grand_total)/grand_total)	* 100
+		melting_total_rate = melt_cost + entry.amount
+		
+		if various_formula > 3.000:	
+			if entry.remark == False:
+				raise osv.except_osv(_('Warning!'),
+						_('Various above 3% Remarks is must !!'))			
+		self.write(cr, uid, ids, {'state': 'confirmed','confirm_user_id': uid, 'confirm_date': time.strftime('%Y-%m-%d %H:%M:%S'),
+		'various': various_formula,'melt_cost': melting_total_rate})
 		return True
 		
 	def entry_cancel(self,cr,uid,ids,context=None):
@@ -187,10 +245,37 @@ class ch_melting_charge_details(osv.osv):
 		'first_addition':fields.float('First Addition',digits=(16,3)),
 		'second_addition':fields.float('Second Addition',digits=(16,3)),
 		'total_weight':fields.float('Total Weight(kg.)',digits=(16,3)),
+		'total_amount':fields.float('Amount',digits=(16,3)),
+		'moc_id': fields.many2one('kg.moc.master','MOC'),
 		'remarks':fields.text('Remarks'),	
 		
 	}
 	
+	def default_get(self, cr, uid, fields, context=None):
+		return context
+	
+	def onchange_purchase_rate(self,cr, uid, ids, product_id,moc_id,first_addition,second_addition, context=None):		
+		cr.execute(''' select line.purchase_price from kg_brandmoc_rate as header
+						left join ch_brandmoc_rate_details line on line.header_id = header.id
+						where header.product_id = %s and line.moc_id = %s
+								  ''',[product_id,moc_id])
+		purchase_price= cr.fetchone()
+		if purchase_price[0]:
+			purchase_price = purchase_price[0]
+		else:
+			purchase_price =0
+		total = first_addition + second_addition
+		
+		amount = total * purchase_price
+		
+		
+		return {'value': {'total_weight': total,'total_amount':amount}}
+		
+	def onchange_addition(self,cr, uid, ids, first_addition,second_addition, context=None):
+		
+		total = first_addition + second_addition
+		
+		return {'value': {'total_weight': total}}
 	
 	
 ch_melting_charge_details()
@@ -204,7 +289,7 @@ class ch_melting_chemistry_details(osv.osv):
 			
 		'header_id':fields.many2one('kg.melting', 'Chemistry Entry', required=True, ondelete='cascade'),							
 		'chemistry_id': fields.many2one('kg.chemical.master','Name', required=True,domain="[('active','=','t')]"),	
-		'required_chemistry':fields.float('Required Chemistry',digits=(16,3)),
+		'required_chemistry':fields.float('Required Chemistry',digits_compute=dp.get_precision('Required Chemistry')),
 		'bath_1':fields.float('Bath 1',digits=(16,3)),
 		'bath_2':fields.float('Bath 2',digits=(16,3)),
 		'final':fields.float('Final',digits=(16,3)),
@@ -233,8 +318,7 @@ class ch_mechanical_properties(osv.osv):
 	
 	def _check_values(self, cr, uid, ids, context=None):
 		entry = self.browse(cr,uid,ids[0])
-		if entry.range_flag == False:
-			print"www"
+		if entry.range_flag == False:			
 			if entry.min > entry.max:
 				return False
 		return True
