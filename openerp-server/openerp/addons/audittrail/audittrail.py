f9cac742760d269cbbc8144c105c0ba85f5064a1
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-06-07 16:11:55 +0530

    Audit rule

--- openerp-server/openerp/addons/audittrail/audittrail.py
+++ openerp-server/openerp/addons/audittrail/audittrail.py
@@ -459,29 +459,32 @@ class audittrail_objects_proxy(object_proxy):
 
             # if at least one modification has been found
             for model_id, resource_id in lines:
-                name = pool.get(model.model).name_get(cr, uid, [resource_id])[0][1]
-                vals = {
-                    'method': method,
-                    'object_id': model_id,
-                    'user_id': uid,
-                    'res_id': resource_id,
-                    'name': name,
-                }
-                if (model_id, resource_id) not in old_values and method not in ('copy', 'read'):
-                    # the resource was not existing so we are forcing the method to 'create'
-                    # (because it could also come with the value 'write' if we are creating
-                    #  new record through a one2many field)
-                    vals.update({'method': 'create'})
-                if (model_id, resource_id) not in new_values and method not in ('copy', 'read'):
-                    # the resource is not existing anymore so we are forcing the method to 'unlink'
-                    # (because it could also come with the value 'write' if we are deleting the
-                    #  record through a one2many field)
-                    vals.update({'method': 'unlink'})
-                # create the audittrail log in super admin mode, only if a change has been detected
-                if lines[(model_id, resource_id)]:
-                    log_id = pool.get('audittrail.log').create(cr, SUPERUSER_ID, vals)
-                    model = pool.get('ir.model').browse(cr, uid, model_id)
-                    self.create_log_line(cr, SUPERUSER_ID, log_id, model, lines[(model_id, resource_id)])
+				if resource_id:
+					name = pool.get(model.model).name_get(cr, uid, [resource_id])[0][1]
+					vals = {
+						'method': method,
+						'object_id': model_id,
+						'user_id': uid,
+						'res_id': resource_id,
+						'name': name,
+					}
+					if (model_id, resource_id) not in old_values and method not in ('copy', 'read'):
+						# the resource was not existing so we are forcing the method to 'create'
+						# (because it could also come with the value 'write' if we are creating
+						#  new record through a one2many field)
+						vals.update({'method': 'create'})
+					if (model_id, resource_id) not in new_values and method not in ('copy', 'read'):
+						# the resource is not existing anymore so we are forcing the method to 'unlink'
+						# (because it could also come with the value 'write' if we are deleting the
+						#  record through a one2many field)
+						vals.update({'method': 'unlink'})
+					# create the audittrail log in super admin mode, only if a change has been detected
+					if lines[(model_id, resource_id)]:
+						log_id = pool.get('audittrail.log').create(cr, SUPERUSER_ID, vals)
+						model = pool.get('ir.model').browse(cr, uid, model_id)
+						self.create_log_line(cr, SUPERUSER_ID, log_id, model, lines[(model_id, resource_id)])
+				else:
+					pass
         return True
 
     def check_rules(self, cr, uid, model, method):
