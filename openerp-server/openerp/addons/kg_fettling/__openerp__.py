578c92a8b3b23d5ef425fa80c3211ecd166325fb
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-06-15 15:42:28 +0530

    Foundry Changes

--- openerp-server/openerp/addons/kg_fettling/__openerp__.py
+++ openerp-server/openerp/addons/kg_fettling/__openerp__.py
@@ -8,7 +8,7 @@
     'name': 'Fettling Process',
     'version': '0.1',
     'author': 'Sangeetha',    
-    'depends' : ['base','kg_stock_inward','kg_production'],
+    'depends' : ['base','kg_stock_inward','kg_production','kg_pouring_log'],
     'data': [
 		'kg_fettling_view.xml'
 		],
