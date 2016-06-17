6b8ff5d889cf00af168e2853938e4494bb95f3a4
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-06-17 09:35:45 +0530

    Pump Model changes Done

--- openerp-server/openerp/addons/kg_machineshop/__openerp__.py
+++ openerp-server/openerp/addons/kg_machineshop/__openerp__.py
@@ -8,7 +8,7 @@
     'name': 'Machine Shop',
     'version': '0.1',
     'author': 'Sangeetha',    
-    'depends' : ['base','kg_stock_inward','kg_production'],
+    'depends' : ['base','kg_stock_inward','kg_production','kg_division_master','kg_schedule','kg_work_order','kg_fettling','kg_stage_master','kg_rejection_master'],
     'data': [
 		'kg_machineshop_view.xml','jasper_report.xml'
 		],
