47ceb6fdf496d09cea6616e1c4f934906cc7a6e0
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-06-22 09:55:19 +0530

    CRM changes Done

--- openerp-server/openerp/addons/kg_crm_enquiry/kg_crm_enquiry.py
+++ openerp-server/openerp/addons/kg_crm_enquiry/kg_crm_enquiry.py
@@ -222,7 +222,7 @@ class kg_crm_enquiry(osv.osv):
 		encoded_user = base64.b64encode(rec_user)
 		encoded_pwd = base64.b64encode(rec_pwd)
 		
-		url = 'http://192.168.1.7/DMS/login.html?xmxyypzr='+encoded_user+'&mxxrqx='+encoded_pwd+'&wo_no='+rec_code	
+		url = 'http://192.168.1.7/DMS/login.html?xmxyypzr='+encoded_user+'&mxxrqx='+encoded_pwd+'&wo_no='+rec_number	
 		
 		return {
 					  'name'	 : 'Go to website',
