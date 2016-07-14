b15dd202e259dd82ebdc00dbf107b3e4ee832941
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-07-14 19:07:44 +0530

    Karthikeyan brandmoc changes done

--- openerp-server/openerp/addons/kg_brandmoc_rate/kg_brandmoc_rate.py
+++ openerp-server/openerp/addons/kg_brandmoc_rate/kg_brandmoc_rate.py
@@ -293,8 +293,8 @@ class ch_brandmoc_rate_details(osv.osv):
 					if ((line['brand_id'] == sub_line['brand_id']) and (line['moc_id'] == sub_line['moc_id'])):		
 						print "pisjoisdjkasdsd",(line['brand_id'] == sub_line['brand_id'])
 						print "dfkluerojwokew",(line['moc_id'] == sub_line['moc_id'])
-						print "dfkluerojwokew",line['moc_id']
-						print "dfkluerojwokew",sub_line['moc_id']
+						print "dfkluerojwokew",line['brand_id']
+						print "dfkluerojwokew",sub_line['brand_id']
 						print "dfkluerojwokew",line['moc_id'] 
 						print "dfkluerojwokew",sub_line['moc_id']
 						return False
