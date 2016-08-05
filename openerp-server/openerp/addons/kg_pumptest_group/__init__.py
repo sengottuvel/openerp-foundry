bb8d3983647f904b20a8a10e04a057b470f971c4
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-08-05 09:58:49 +0530

    Two Masters added changes done

---
+++ openerp-server/openerp/addons/kg_pumptest_group/__init__.py
@@ -0,0 +1,1 @@
+import kg_pumptest_group
---
+++ openerp-server/openerp/addons/kg_pumptest_group/__openerp__.py
@@ -0,0 +1,18 @@
+##############################################################################
+#
+#   This is New Customized Pump Model Master
+#
+##############################################################################
+
+{
+    'name': 'Pump Test Group Master',
+    'version': '0.1',
+    'author': 'karthikeyan',
+    'category': 'BASE',        
+    'depends' : ['base','kg_pumptest_type'],
+    'data': ['kg_pumptest_group_view.xml'],
+    'css': ['static/src/css/state.css'], 
+    'auto_install': False,
+    'installable': True,
+}
+
