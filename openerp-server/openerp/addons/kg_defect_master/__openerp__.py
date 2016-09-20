7eb25c8ec58f5c70091485477c4ab253d5832cec
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-09-20 20:07:40 +0530

    GRN changes done

---
+++ openerp-server/openerp/addons/kg_defect_master/__openerp__.py
@@ -0,0 +1,20 @@
+##############################################################################
+#
+#   This is New Customized Defect Type Master
+#
+##############################################################################
+
+{
+
+    'name': 'Defect Master',
+    'version': '0.1',
+    'author': 'Thangaraj',
+    'category': 'CRM',
+    'depends' : ['base'],
+    'data': ['kg_defect_master_view.xml'],
+    'css': ['static/src/css/state.css'], 
+    'auto_install': False,
+    'installable': True,
+    
+}
+
