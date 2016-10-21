ff43787dfe186f3b02171f92f9a2d058c63089a4
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-10-21 16:34:56 +0530

    Mail Setting changes in Trail by Karthikeyan

--- openerp-server/openerp/addons/kg_mail_settings/kg_mail_settings.py
+++ openerp-server/openerp/addons/kg_mail_settings/kg_mail_settings.py
@@ -3,24 +3,26 @@ import re
 from openerp import tools
 from openerp.osv import osv, fields
 from openerp.tools.translate import _
-import time
 import openerp.addons.decimal_precision as dp
 import netsvc
 from datetime import date
-from datetime import datetime
 from datetime import timedelta
 from dateutil import relativedelta
 import datetime
 import calendar
+import time
+from datetime import datetime
+import base64
+dt_time = time.strftime('%m/%d/%Y %H:%M:%S')
 
 class kg_mail_settings(osv.osv):
 	_name = "kg.mail.settings"
 	_description = "Mail General Settings"
 
 	_columns = {
-		'creation_time':fields.date('Creation Date',readonly=True),
-		'created_by':fields.many2one('res.users', 'Created By',readonly=True),
-		'active':fields.boolean('Active'),
+		
+		'company_id': fields.many2one('res.company', 'Company Name',readonly=True),
+		'active':fields.boolean('Active',readonly=False),
 		'doc_name': fields.many2one('ir.model','Document Name',readonly=True, states={'draft':[('readonly',False)]}),
 		'name':fields.char('From Email-ID',required = True,readonly=True, states={'draft':[('readonly',False)]}),
 		'line_ids':fields.one2many('ch.mail.settings.line','header_id',readonly=False),
@@ -32,10 +34,21 @@ class kg_mail_settings(osv.osv):
 		'sch_interval': fields.selection([('daily','Daily'),('15_days','15 Days Once'),('monthly','Monthly'),('yearly','Yearly')],'Interval',readonly=True, states={'draft':[('readonly',False)]}),
 		'sch_type': fields.selection([('transaction','Transaction Mail'),('scheduler','Scheduler Mail')],'Mail Type', required=True, readonly=True, states={'draft':[('readonly',False)]}),
 		
+		### Entry Info ###
+		'creation_time':fields.datetime('Creation Date',readonly=True),
+		'created_by':fields.many2one('res.users', 'Created By',readonly=True),
+		'confirm_date': fields.datetime('Confirmed Date', readonly=True),
+		'confirm_user_id': fields.many2one('res.users', 'Confirmed By', readonly=True),
+		'ap_rej_date': fields.datetime('Approved/Reject Date', readonly=True),
+		'ap_rej_user_id': fields.many2one('res.users', 'Approved/Reject By', readonly=True),		
+		'update_date': fields.datetime('Last Updated Date', readonly=True),
+		'update_user_id': fields.many2one('res.users', 'Last Updated By', readonly=True),	
+		
 	}
 	
 	_defaults = {
-		'creation_time': lambda * a: time.strftime('%Y-%m-%d'),
+		'company_id': lambda self,cr,uid,c: self.pool.get('res.company')._company_default_get(cr, uid, 'kg.mail.settings', context=c),
+		'creation_time': fields.datetime.now,
 		'state':'draft',
 		'active':True,
 		'created_by': lambda self, cr, uid, c: self.pool.get('res.users').browse(cr, uid, uid, c).id ,
@@ -43,7 +56,7 @@ class kg_mail_settings(osv.osv):
 	}
 	 
 	def approve_entry(self, cr, uid, ids,context=None):		
-		self.write(cr,uid,ids,{'state':'validate'})
+		self.write(cr,uid,ids,{'state':'validate','ap_rej_user_id': uid, 'ap_rej_date': time.strftime('%Y-%m-%d %H:%M:%S')})
 		return True
 		
 	def confirm_entry(self, cr, uid, ids, context=None):
@@ -58,7 +71,7 @@ class kg_mail_settings(osv.osv):
 			today_date = datetime.datetime.today()
 			dup_rec.write({'active': False})
 			dup_rec.write({'expiry_date':today_date})"""
-		self.write(cr,uid,ids,{'state':'confirm'})	
+		self.write(cr,uid,ids,{'state':'confirm','confirm_user_id': uid, 'confirm_date': time.strftime('%Y-%m-%d %H:%M:%S')})	
 		return True
 					
 	def draft_entry(self, cr, uid, ids, context=None):
@@ -92,6 +105,9 @@ class kg_mail_settings(osv.osv):
 		if not to_add:
 			raise osv.except_osv(_('Warning!'), _('Atleast One TO mail ID should be given!!'))
 		return True
+	def write(self, cr, uid, ids, vals, context=None):
+		vals.update({'update_date': time.strftime('%Y-%m-%d %H:%M:%S'),'update_user_id':uid})
+		return super(kg_mail_settings, self).write(cr, uid, ids, vals, context)
 			
 	_constraints = [        
               
