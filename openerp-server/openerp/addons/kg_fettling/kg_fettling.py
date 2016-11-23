9e7e2d5e6d752bfa08dc9e5a7bb14fa3d6201eae
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-11-23 09:51:24 +0530

    BOM changes done by karthikeyan

--- openerp-server/openerp/addons/kg_fettling/kg_fettling.py
+++ openerp-server/openerp/addons/kg_fettling/kg_fettling.py
@@ -603,6 +603,8 @@ class kg_fettling(osv.osv):
 			
 				### Status Updation ###
 				
+				print"entry_rec.production_id.id",entry_rec.production_id.id
+				
 				### Schedule List Updation ###
 				production_obj = self.pool.get('kg.production')
 				cr.execute(''' update kg_production set state = 'moved_to_ms' where id = %s ''',[entry_rec.production_id.id])
