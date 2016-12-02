f8af67678430b0535204eaed91b66f4e0303953c
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-12-02 10:05:28 +0530

    Packing Type Master done by karthikeyan

---
+++ openerp-server/openerp/addons/kg_packing_type/__openerp__.py
@@ -0,0 +1,18 @@
+##############################################################################
+#
+#   This is New Customized Pump Model Master
+#
+##############################################################################
+
+{
+    'name': 'Packing Type Master',
+    'version': '0.1',
+    'author': 'Karthikeyan S',
+    'category': 'BASE',        
+    'depends' : ['base'],
+    'data': ['kg_packing_type_view.xml'],
+    'css': ['static/src/css/state.css'], 
+    'auto_install': False,
+    'installable': True,
+}
+
