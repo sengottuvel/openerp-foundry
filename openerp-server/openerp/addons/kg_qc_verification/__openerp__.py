44fbb7f377ead0b05e025cebbac4bc9c1b9aa764
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2016-12-15 12:46:15 +0530

    QC Verification Changes done by Karthikeyan

--- openerp-server/openerp/addons/kg_qc_verification/__openerp__.py
+++ openerp-server/openerp/addons/kg_qc_verification/__openerp__.py
@@ -11,7 +11,8 @@
     'depends' : ['base', 'product','kg_pumpmodel_master', 'kg_pattern_master', 'kg_moc_master', 'kg_division_master', 'kg_schedule',
                 'kg_stock_inward'],
     'data': [
-		'kg_qc_verification_view.xml'
+		'kg_qc_verification_view.xml',
+		'jasper_report.xml',
 		],
     'css': ['static/src/css/state.css'], 
     'auto_install': False,
