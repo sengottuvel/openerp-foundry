7495291e777e84968af83e7a4b622b241cd460b1
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-01-31 19:35:32 +0530

    employee master update

--- openerp-server/openerp/addons/kg_employee/__openerp__.py
+++ openerp-server/openerp/addons/kg_employee/__openerp__.py
@@ -9,7 +9,7 @@
     'version': '0.1',
     'author': 'Sengottuvelu',
     'category': 'BASE',        
-    'depends' : ['base','hr'],
+    'depends' : ['base','hr','kg_employee_category'],
     'data': [
 			'kg_employee_view.xml',
 			'default_data.xml'
