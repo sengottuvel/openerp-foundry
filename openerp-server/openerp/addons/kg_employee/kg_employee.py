229630166b7547a4c3eda78de2a44aee23a2cc26
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-01-31 18:55:28 +0530

    employee master changed as per ticket 842

--- openerp-server/openerp/addons/kg_employee/kg_employee.py
+++ openerp-server/openerp/addons/kg_employee/kg_employee.py
@@ -3,15 +3,10 @@ from openerp.osv import osv, fields
 from openerp.tools.translate import _
 import time
 import openerp.addons.decimal_precision as dp
-from dateutil import relativedelta as rdelta
 from datetime import date
-
 from datetime import datetime
 import re
 import math
-
-from dateutil import relativedelta
-from dateutil import relativedelta as rdelta
 dt_time = time.strftime('%m/%d/%Y %H:%M:%S')
 today = datetime.now()
 
@@ -65,8 +60,8 @@ class kg_employee(osv.osv):
 	
 		### Basic Info
 			
-		'code': fields.char('Code', size=10, required=True),		
-		'status': fields.selection([('draft','Draft'),('confirmed','WFA'),('approved','Approved'),('reject','Rejected'),('cancel','Cancelled'),('relieve','Relieve In-Progress'),('resigned','Resigned')],'Status', readonly=True),
+		'code': fields.char('Code', size=4, required=True),		
+		'status': fields.selection([('draft','Draft'),('confirmed','WFA'),('approved','Approved'),('reject','Rejected'),('cancel','Cancelled')],'Status', readonly=True),
 		'notes': fields.text('Notes'),
 		'remark': fields.text('Approve/Reject'),
 		'cancel_remark': fields.text('Cancel'),
@@ -104,18 +99,18 @@ class kg_employee(osv.osv):
 		'father_occ': fields.char('Father Occupation',size=128),
 		'mother_occ': fields.char('Mother Occupation', size=128),
 		'pre_add': fields.char('Present Address', size=256, ),
-		'pre_city': fields.many2one('res.city', 'City',select=True),
-		'pre_state_id': fields.many2one('res.country.state', 'State',domain=[('active','=',True)] ),
+		'pre_city': fields.char('City', size=128, ),
+		'pre_state_id': fields.many2one('res.country.state', 'State', ),
 		'pre_country_id': fields.many2one('res.country', 'Country', ),
-		'pre_pin_code': fields.integer('Postal Code',size=8),
-		'pre_phone_no': fields.char('Phone Number',size=15),
+		'pre_pin_code': fields.integer('Postal Code',),
+		'pre_phone_no': fields.char('Phone Number',),
 		'same_pre_add': fields.boolean('Same as Present Address'),
 		'permanent_add': fields.char('Permanent Address', size=256,),
-		'city_id': fields.many2one('res.city', 'City', ),
+		'city_id': fields.char('City', size=128,),
 		'state_id': fields.many2one('res.country.state', 'State',),
 		'country_id': fields.many2one('res.country', 'Country',),
-		'pin_code': fields.integer('Postal Code',size=8),
-		'phone_no': fields.char('Phone Number',size=15),
+		'pin_code': fields.integer('Postal Code',),
+		'phone_no': fields.char('Phone Number',),
 		'ann_date': fields.date('Anniversery Date'),
 		'wife_hus_name': fields.char('Wife/Husband Name'),
 		'adhar_data': fields.binary('Adhar Copy'),
@@ -129,18 +124,6 @@ class kg_employee(osv.osv):
 		'sample_data4': fields.char('Voter Copy'),
 		'sample_data5': fields.char('Voter Copy'),
 		'sample_data6': fields.char('Voter Copy'),
-		
-		'att_code': fields.char('Attendance Code'),
-		'join_mode': fields.selection([('new','New'),('rejoin','Re-Join')],'Joining Mode'),
-		'mode_of_att': fields.selection([('manual','Manual'),('electronic','Electronic'),('both','Both')],'Mode of Attendance'),
-		'personal_email': fields.char('Personal Email'),
-		'emp_categ_id':fields.many2one('kg.employee.category','Employee Category'),
-		'bank_acc_no': fields.char('Bank Account No',size=15),
-		'children_1': fields.integer('Children',),
-		'nationality': fields.char('Nationality',),
-		'wrk_address': fields.char('Working Address',),
-		'dep_id':fields.many2one('kg.depmaster','Department'),
-		'pan_no':fields.char('Pan No',size=12),
 		
 		## Child Tables Declaration	
 			
@@ -168,19 +151,12 @@ class kg_employee(osv.osv):
 		'join_date':lambda * a: time.strftime('%Y-%m-%d %H:%M:%S'),
 		'modify': 'no',
 		'entry_mode': 'manual',
-		'join_mode': 'new',
-		'wrk_address': 'SAM TURBO INDUSTRY PRIVATE LIMITED',
-		'nationality': 'INDIA',
 		
 	}
 	
 	_sql_constraints = [
 	
 		('code', 'unique(code)', 'Employee Code must be unique !!'),
-		('adhar_data', 'unique(adhar_data)', 'Adhar No must be unique !!'),
-		('pan_data', 'unique(pan_data)', 'PAN No must be unique !!'),
-		('bank_acc_no', 'unique(bank_acc_no)', 'Bank Account No must be unique !!'),
-		('mobile_phone', 'unique(mobile_phone)', 'Mobile No must be unique per employee !!'),
 	]
 	
 	
@@ -188,70 +164,33 @@ class kg_employee(osv.osv):
 	
 	def entry_cancel(self,cr,uid,ids,context=None):
 		rec = self.browse(cr,uid,ids[0])
-		if rec.status =='approved':
-			if rec.cancel_remark:
-				self.write(cr, uid, ids, {'status': 'cancel','cancel_user_id': uid, 'cancel_date': time.strftime('%Y-%m-%d %H:%M:%S')})
-			else:
-				raise osv.except_osv(_('Cancel remark is must !!'),
-					_('Enter the remarks in Cancel remarks field !!'))
+		if rec.cancel_remark:
+			self.write(cr, uid, ids, {'status': 'cancel','cancel_user_id': uid, 'cancel_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+		else:
+			raise osv.except_osv(_('Cancel remark is must !!'),
+				_('Enter the remarks in Cancel remarks field !!'))
 		return True
 
 	def entry_confirm(self,cr,uid,ids,context=None):
-		rec = self.browse(cr,uid,ids[0])
-		if rec.status =='draft':
-			self.write(cr, uid, ids, {'status': 'confirmed','confirm_user_id': uid, 'confirm_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+		self.write(cr, uid, ids, {'status': 'confirmed','confirm_user_id': uid, 'confirm_date': time.strftime('%Y-%m-%d %H:%M:%S')})
 		return True
 		
 	def entry_draft(self,cr,uid,ids,context=None):
-		if rec.status == 'cancel':			
-			self.write(cr, uid, ids, {'status': 'draft'})
+		self.write(cr, uid, ids, {'status': 'draft'})
 		return True
 
 	def entry_approve(self,cr,uid,ids,context=None):
 		rec = self.browse(cr,uid,ids[0])
-		if rec.status =='confirmed':
-			emp_obj_1 = self.pool.get('hr.contract')
-			emp_categ_obj = self.pool.get('kg.employee.category')
-			emp_categ_line = self.pool.get('ch.salary.policy')
-			contract_salary = self.pool.get('ch.kg.contract.salary')
-			emp_categ_line_1 = emp_categ_obj.browse(cr,uid,rec.emp_categ_id.id)
-					
-			emp_vals = {
-
-					'employee_id': rec.id,
-					'code':rec.code,
-					'dep_id':rec.dep_id.id,
-					'job_id':rec.job_id.id,
-					'emp_categ_id':rec.emp_categ_id.id,
-					'shift_id':emp_categ_line_1.shift_id.id,
-
-					}
-			att_id = emp_obj_1.create(cr,uid,emp_vals)
-			emp_obj = self.pool.get('hr.contract')
-			emp_con_1 = emp_obj.search(cr,uid,[('employee_id','=',rec.id)])
-			for i in emp_categ_line_1.line_id_3:
-
-				emp_con_vals = {
-
-					'header_id_salary': emp_con_1[0],
-					'salary_type':i.allow_deduction_id.id,
-					'amt_type':i.type,
-					'salary_amt':i.value,
-
-					}
-				salary_policy = contract_salary.create(cr,uid,emp_con_vals)
-
-			self.write(cr, uid, ids, {'status': 'approved','ap_rej_user_id': uid, 'ap_rej_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+		self.write(cr, uid, ids, {'status': 'approved','ap_rej_user_id': uid, 'ap_rej_date': time.strftime('%Y-%m-%d %H:%M:%S')})
 		return True
 
 	def entry_reject(self,cr,uid,ids,context=None):
 		rec = self.browse(cr,uid,ids[0])
-		if rec.status =='confirmed':
-			if rec.remark:
-				self.write(cr, uid, ids, {'status': 'reject','ap_rej_user_id': uid, 'ap_rej_date': time.strftime('%Y-%m-%d %H:%M:%S')})
-			else:
-				raise osv.except_osv(_('Rejection remark is must !!'),
-					_('Enter the remarks in rejection remark field !!'))
+		if rec.remark:
+			self.write(cr, uid, ids, {'status': 'reject','ap_rej_user_id': uid, 'ap_rej_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+		else:
+			raise osv.except_osv(_('Rejection remark is must !!'),
+				_('Enter the remarks in rejection remark field !!'))
 		return True
 		
 	def unlink(self,cr,uid,ids,context=None):
@@ -284,18 +223,33 @@ class kg_employee(osv.osv):
 		
 	###Validataions###
 	
-	def _dob_validation(self, cr, uid, ids, context=None):
-		rec = self.browse(cr, uid, ids[0])
-		today = date.today()
-		if rec.birthday:
-			dob_date = datetime.strptime(rec.birthday,'%Y-%m-%d').date()
-			b_date = datetime.strptime(rec.birthday, '%Y-%m-%d')
-			age=((datetime.today() - b_date).days/365)
-			if dob_date >= today or age<18:
+	def _Validation(self, cr, uid, ids, context=None):
+		flds = self.browse(cr , uid , ids[0])
+		name_special_char = ''.join( c for c in flds.name if  c in '!@#$%^~*{}?+/=' )		
+		if name_special_char:
+			return False		
+		return True	
+		
+	def _CodeValidation(self, cr, uid, ids, context=None):
+		flds = self.browse(cr , uid , ids[0])	
+		if flds.code:		
+			code_special_char = ''.join( c for c in flds.code if  c in '!@#$%^~*{}?+/=' )		
+			if code_special_char:
 				return False
-			return True
-		else:
-			pass
+		return True		
+	
+	#~ def _dob_validation(self, cr, uid, ids, context=None):
+		#~ rec = self.browse(cr, uid, ids[0])
+		#~ today = date.today()
+		#~ if rec.birthday:
+			#~ dob_date = datetime.strptime(rec.birthday,'%Y-%m-%d').date()
+			#~ b_date = datetime.strptime(rec.birthday, '%Y-%m-%d')
+			#~ age=((datetime.today() - b_date).days/365)
+			#~ if dob_date >= today or age<18:
+				#~ return False
+			#~ return True
+		#~ else:
+			#~ pass
 			
 	def _joindate_validation(self, cr, uid, ids, context=None):
 		rec = self.browse(cr, uid, ids[0])
@@ -306,51 +260,29 @@ class kg_employee(osv.osv):
 		return True
 	
 	_constraints = [
-
-		(_dob_validation, 'Future dates/Age less than 18 are not allowed for Date of Birth!!', [' ']),		
+	
+		(_Validation, 'Special Characters Not Allowed !!!', ['Check Name']),
+		(_CodeValidation, 'Special Characters Not Allowed !!!', ['Check Code']),		
+		#~ (_dob_validation, 'Future dates/Age less than 18 are not allowed for Date of Birth!!', [' ']),		
 		(_joindate_validation, 'Future dates are not allowed for Date of Joining!!', [' ']),		
 		
 	]
 	
 	## Module Requirement
 	
-	def onchange_address(self, cr, uid, ids,same_pre_add,pre_add,pre_city,pre_state_id,country_id,pre_pin_code,pre_phone_no,context=None):
-		value = {'permanent_add':'','city_id':'','state_id':'','pre_country_id':'','pin_code':'','phone_no':''}
+	def onchange_address(self, cr, uid, ids,same_pre_add,pre_add,pre_city,pre_state_id,pre_country_id,pre_pin_code,pre_phone_no,context=None):
+		value = {'permanent_add':'','city':'','state_id':'','country_id':'','pin_code':'','phone_no':''}
 		if same_pre_add == True:
 			value = {
 					'permanent_add':pre_add,
 					'city_id':pre_city,
 					'state_id':pre_state_id,
-					'pre_country_id':country_id,
+					'country_id':pre_country_id,
 					'pin_code':pre_pin_code,
 					'phone_no':pre_phone_no
 					}
 					
 		return {'value': value}
-		
-	def onchange_city(self, cr, uid, ids, pre_city, context=None):
-		value = {'pre_city':'','pre_state_id':''}
-		if pre_city:
-			state_id = self.pool.get('res.city').browse(cr, uid, pre_city, context).state_id.id
-			country_id = self.pool.get('res.city').browse(cr, uid,pre_city, context).country_id.id
-			return {'value':{'pre_state_id':state_id,'pre_country_id':country_id}}
-		return {}
-		
-	def emp_relieve(self,cr,uid,ids,context=None):
-		rec = self.browse(cr,uid,ids[0])
-		if rec.status=='approved':
-			self.write(cr, uid, ids, {'status': 'relieve'})
-		return True
-		
-	def emp_resigned(self,cr,uid,ids,context=None):
-		rec = self.browse(cr,uid,ids[0])
-		if rec.status=='approved':
-			if rec.releaving_date and rec.releaving_reason:
-				self.write(cr, uid, ids, {'status': 'resigned'})
-			else:
-				raise osv.except_osv(_('Warning!'),
-							_('Please Enter the Releaving Date and Releaving Reason'))
-		return True
 
 kg_employee()
 
@@ -362,7 +294,7 @@ class ch_kg_employee_ref(osv.osv):
 	
 				'header_id_ref' : fields.many2one('hr.employee','Header ID Ref'),
 				'name':fields.char('Name',required = True),
-				'contact_no':fields.char('Contact Number',required = True,size=15),
+				'contact_no':fields.char('Contact Number',required = True),
 				'relation_ship':fields.char('Relation Ship',required = True),
 				'designation':fields.char('Designation',),
 				'address':fields.text('Address',required = True)
@@ -412,7 +344,7 @@ class ch_kg_employee_ref_emp(osv.osv):
 	_columns = {
 	
 	'header_id_ref_emp':fields.many2one('hr.employee','Header ID Ref Emp'),
-	'work_exp': fields.char('Experience (Y / M / D)', size=128,required=True),
+	'work_exp': fields.char('Experience', size=128,required=True),
 	'cmp_name': fields.char('Company', size=128 ,required=True),
 	'position': fields.char('Position', size=128 ,required=True),
 	'spec': fields.char('Specialization', size=128 ,required=True),
@@ -423,24 +355,12 @@ class ch_kg_employee_ref_emp(osv.osv):
 	
 	###Validations###
 	
-	
 	def _fromto_date_validation(self, cr, uid, ids, context=None):
 		rec = self.browse(cr, uid, ids[0])
 		today = date.today()
 		start_date = datetime.strptime(rec.from_date,'%Y-%m-%d').date()
 		end_date = datetime.strptime(rec.to_date,'%Y-%m-%d').date()
-		if start_date > today:
-			raise osv.except_osv(_('Warning !!'),
-					_('Current Date not accepted in Start Date !!'))
-		if end_date > today:
-			raise osv.except_osv(_('Warning !!'),
-					_('Current Date not accepted in End Date !!'))
-		if start_date == end_date:
-			raise osv.except_osv(_('Warning !!'),
-					_('Start and End Date should not be equal !!'))
-		if start_date > end_date:
-			raise osv.except_osv(_('Warning !!'),
-					_('End Date should be greater than Start Date !!'))
+		if start_date > today or end_date > today or start_date == end_date:
 			return False
 		return True
 
@@ -449,17 +369,6 @@ class ch_kg_employee_ref_emp(osv.osv):
 		(_fromto_date_validation, 'From date and To Date should be less than current date for Work History !!',[' ']),
 
 		]  
-	
-	def onchange_to_date(self, cr, uid, ids,to_date,from_date,context=None):
-		value = {'from_date':'','to_date':''}
-		d1 =  datetime.strptime(from_date, "%Y-%m-%d")
-		d2 =  datetime.strptime(to_date, "%Y-%m-%d")
-		rd = rdelta.relativedelta(d1,d2)
-		years= "{0.years} / {0.months} / {0.days}".format(rd)
-		value = {
-				'work_exp':years,
-				}
-		return {'value': value}
 		
 ch_kg_employee_ref_emp()
 
@@ -514,15 +423,15 @@ class ch_kg_employee_his(osv.osv):
 		'pre_city': fields.char('City', size=128, ),
 		'pre_state_id': fields.many2one('res.country.state', 'State', ),
 		'pre_country_id': fields.many2one('res.country', 'Country', ),
-		'pre_pin_code': fields.integer('Postal Code',size=8),
-		'pre_phone_no': fields.char('Phone Number',size=15),
+		'pre_pin_code': fields.integer('Postal Code',),
+		'pre_phone_no': fields.char('Phone Number',),
 		'same_pre_add': fields.boolean('Same as Present Address'),
 		'permanent_add': fields.char('Permanent Address', size=256,),
 		'city_id': fields.char('City', size=128,),
 		'state_id': fields.many2one('res.country.state', 'State',),
 		'country_id': fields.many2one('res.country', 'Country',),
-		'pin_code': fields.integer('Postal Code',size=8),
-		'phone_no': fields.char('Phone Number',size=15),
+		'pin_code': fields.integer('Postal Code',),
+		'phone_no': fields.char('Phone Number',),
 		'ann_date': fields.date('Anniversery Date'),
 		'wife_hus_name': fields.char('Wife/Husband Name'),
 		'adhar_data': fields.binary('Adhar Copy'),
@@ -541,7 +450,6 @@ class ch_kg_employee_his(osv.osv):
 		'remaining_leaves': fields.integer('Remaining Leaves', readonly=True),
 		'identification_id': fields.char('Adhar ID', readonly=True),
 		'bank_account_id': fields.char('bank name', readonly=True),
-		'children_1': fields.integer('Children',),
 	}
 	
 	_defaults = {
@@ -563,7 +471,7 @@ class ch_kg_employee_ref_his(osv.osv):
 	
 				'header_id_ref_his' : fields.many2one('hr.employee','Header ID Ref History'),
 				'name':fields.char('Name'),
-				'contact_no':fields.char('Contact Number',size=15),
+				'contact_no':fields.char('Contact Number'),
 				'relation_ship':fields.char('Relation Ship'),
 				'designation':fields.char('Designation'),
 				'address':fields.text('Address')
