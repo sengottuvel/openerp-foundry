f989ac142c675a39daf1510db87912d6250a157f
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-02-09 19:54:52 +0530

    Offer term master added by Thangaraj

---
+++ openerp-server/openerp/addons/kg_offer_term/__openerp__.py
@@ -0,0 +1,20 @@
+##############################################################################
+#
+#   This is New Master Module
+#
+##############################################################################
+
+{
+    'name': 'Offer Terms',
+    'version': '0.1',
+    'author': 'Thangaraj',
+    'category': 'BASE',        
+    'depends' : ['base'],
+    'data': [
+			'kg_offer_term_view.xml',
+			],
+			
+    'css': ['static/src/css/state.css'], 
+    'auto_install': False,
+    'installable': True,
+}
