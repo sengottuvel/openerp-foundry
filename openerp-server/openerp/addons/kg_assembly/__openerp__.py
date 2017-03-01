fd0056c90deb780f761439ccd84c42189c7acb68
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-03-01 10:29:26 +0530

    part qap changes done by sangeetha

--- openerp-server/openerp/addons/kg_assembly/__openerp__.py
+++ openerp-server/openerp/addons/kg_assembly/__openerp__.py
@@ -8,7 +8,7 @@
     'name': 'Assembly Process',
     'version': '0.1',
     'author': 'Sangeetha',    
-    'depends' : ['base','kg_ms_stores'],
+    'depends' : ['base','kg_ms_stores','kg_part_qap'],
     'data': [
 		'kg_assembly_view.xml'
 		],
