e9e6b94d15b120dcdf9e100888ba371930a91d61
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-03-29 20:02:25 +0530

    General term master changes done by Thangaraj

---
+++ openerp-server/openerp/addons/kg_item_quality/__openerp__.py
@@ -0,0 +1,20 @@
+##############################################################################
+#
+#   This is New Customized PO Note Master
+#
+##############################################################################
+
+{
+
+    'name': 'Item Quality Master',
+    'version': '0.1',
+    'author': 'thangaraj',
+    'category': 'Purchase Order',        
+    'depends' : ['base'],
+    'data': ['kg_item_quality_master_view.xml'],
+    'css': ['static/src/css/state.css'], 
+    'auto_install': False,
+    'installable': True,
+    
+}
+
