d9df0eb27ccba503037ce1e0dfae5aaace7daa91
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-03-29 17:29:26 +0530

    Added the module statement import

--- openerp-server/openerp/addons/kg_bank_statement_import/kg_bank_statement_import.py
+++ openerp-server/openerp/addons/kg_bank_statement_import/kg_bank_statement_import.py
@@ -6,12 +6,20 @@ import openerp.addons.decimal_precision as dp
 from datetime import datetime
 import re
 import math
+import struct 
+import base64
+import codecs
+import StringIO
+import csv
+import psycopg2
 dt_time = time.strftime('%m/%d/%Y %H:%M:%S')
 
-class kg_bank_statement(osv.osv):
+from xlrd import open_workbook
+
+class kg_bank_statement_import(osv.osv):
 	
-	_name = "kg.bank.statement"
-	_description = "Bank Statement"
+	_name = "kg.bank.statement.import"
+	_description = "Bank Statement Import"
 	
 	### Version 0.1
 	
@@ -29,7 +37,7 @@ class kg_bank_statement(osv.osv):
 					JOIN information_schema.constraint_column_usage ccu ON ccu.constraint_name = tc.constraint_name
 					WHERE constraint_type = 'FOREIGN KEY'
 					AND ccu.table_name='%s')
-					as sam  """ %('kg_bank_statement'))
+					as sam  """ %('kg_bank_statement_import'))
 					data = cr.dictfetchall()	
 					if data:
 						for var in data:
@@ -77,20 +85,15 @@ class kg_bank_statement(osv.osv):
 		
 		## Module Requirement Info
 		'acc_journal_id':fields.many2one('account.journal','Bank Account'),
-		'clear_date':fields.date('Clearing Date'),
-		'open_bal':fields.float('Opening Balance'),
-		'calc_close_bal':fields.float('Calculated Closing Balance'),
-		'actual_close_bal':fields.float('Actual Closing Balance'),
-		'open_bal_type':fields.selection([('dr','Dr'),('cr','Cr')],'Dr/Cr'),
-		'calc_close_bal_type':fields.selection([('dr','Dr'),('cr','Cr')],'Dr/Cr'),
-		'actual_close_bal_type':fields.selection([('dr','Dr'),('cr','Cr')],'Dr/Cr'),
-		'division_id':fields.many2one('kg.division.master','Division'),
-		'acct_name':fields.many2one('account.account','Account Name'),
-		'bnk_imp_id':fields.many2one('kg.bank.statement.import','Bank Statement Import'),
+		'from_date':fields.date('From Date'),
+		'to_date':fields.date('To Date'),
+		'file_type':fields.selection([('excel','Excel'),('csv','CSV')],'File Type'),
+		'file_data':fields.binary('Choose File (Excel/CSV)'),
+		'filename':fields.char('Filename'),
 		
 		## Child Tables Declaration
 		
-		'line_id':fields.one2many('ch.bank.statement','header_id','Line id',readonly=True, states={'draft':[('readonly',False)]}),
+		'line_id':fields.one2many('ch.bank.statement.import','header_id','Line id'),
 		
 				
 	}
@@ -106,11 +109,7 @@ class kg_bank_statement(osv.osv):
 		'entry_mode': 'manual',
 		
 	}
-	
-	_sql_constraints = [
-	
-		('name', 'unique(name)', 'Name must be unique per Company !!'),
-	]
+
 	
 	## Basic Needs	
 	
@@ -128,17 +127,56 @@ class kg_bank_statement(osv.osv):
 
 	def entry_confirm(self,cr,uid,ids,context=None):
 		rec = self.browse(cr,uid,ids[0])
+		if rec.to_date < rec.from_date:
+			raise osv.except_osv(_('Warning !!'),
+				_('From and To Date should not be less than From Date !!'))
 		if not rec.line_id:
 			raise osv.except_osv(_('Warning!'),
 				_('Bank Statement Details should not be empty !!'))
 		for line_ids in rec.line_id:
+			trns_date = datetime.strptime(line_ids.trans_date,'%Y-%m-%d')
+			if line_ids.trans_date < rec.from_date:
+				raise osv.except_osv(_('Warning!'),
+					_('Transaction Date %s is less than the From Date !!'%(trns_date.strftime('%d-%m-%Y'))))
+			if line_ids.trans_date > rec.to_date:
+				trns_date = datetime.strptime(line_ids.trans_date,'%Y-%m-%d')
+				raise osv.except_osv(_('Warning!'),
+					_('Transaction Date %s is greater than the To Date !!'%(trns_date.strftime('%d-%m-%Y'))))
 			if (line_ids.debit == 0.00 and line_ids.credit == 0.00):
 				raise osv.except_osv(_('Warning!'),
-					_('Debit and credit should not be zero for Cheque No/Ref No %s !!'%(line_ids.cheque_no)))
+					_('Debit and credit should not be zero for Transaction Date %s !!'%(trns_date.strftime('%d-%m-%Y'))))
 			elif (line_ids.debit != 0.00 and line_ids.credit != 0.00):
 				raise osv.except_osv(_('Warning!'),
-					_('Both Debit and credit should not contain value for Cheque No/Ref No %s !!'%(line_ids.cheque_no)))
+					_('Both Debit and credit should not contain value for Transaction Date %s !!'%(trns_date.strftime('%d-%m-%Y'))))
 		if rec.state == 'draft':
+			from itertools import groupby
+			total_dates = [line_ids.trans_date for line_ids in rec.line_id]
+			src_dups_bnk_entry = self.pool.get('kg.bank.statement').search(cr,uid,[('bnk_imp_id','=',rec.id)])
+			if src_dups_bnk_entry:
+				cr.execute('''delete from kg_bank_statement where bnk_imp_id = '%d' '''%(rec.id))
+			else:
+				pass
+			for dates in list(set(total_dates)):
+				self.pool.get('kg.bank.statement').create(cr,uid,
+						{
+							'clear_date':dates,
+							'acc_journal_id':rec.acc_journal_id.id,
+							'bnk_imp_id':rec.id,
+							'entry_mode':'auto'
+						})
+				src_bank_entry = self.pool.get('kg.bank.statement').search(cr,uid,[('clear_date','=',dates),('bnk_imp_id','=',rec.id)])
+				bank_ent_rec = self.pool.get('kg.bank.statement').browse(cr,uid,src_bank_entry[0])
+				src_line_ids = self.pool.get('ch.bank.statement.import').search(cr,uid,[('trans_date','=',dates),('header_id','=',rec.id)])
+				for idss in src_line_ids:
+					src_rec = self.pool.get('ch.bank.statement.import').browse(cr,uid,idss)
+					self.pool.get('ch.bank.statement').create(cr,uid,
+						{
+							'cheque_no':src_rec.ref_no,
+							'header_id':bank_ent_rec.id,
+							'narration':src_rec.narration,
+							'debit':src_rec.debit,
+							'credit':src_rec.credit,
+						})
 			self.write(cr, uid, ids, {'state': 'confirmed','confirm_user_id': uid, 'confirm_date': time.strftime('%Y-%m-%d %H:%M:%S')})
 		else:
 			pass
@@ -184,30 +222,68 @@ class kg_bank_statement(osv.osv):
 		
 	def write(self, cr, uid, ids, vals, context=None):
 		vals.update({'update_date': time.strftime('%Y-%m-%d %H:%M:%S'),'update_user_id':uid})
-		return super(kg_bank_statement, self).write(cr, uid, ids, vals, context)	
-
+		return super(kg_bank_statement_import, self).write(cr, uid, ids, vals, context)	
+	
 	## Module Requirement
 	
-	def onchange_account(self,cr,uid,ids,acc_journal_id,acct_name,context=None):
-		led_rec = self.pool.get('account.journal').browse(cr,uid,acc_journal_id)
-		value = {'acct_name':led_rec.default_debit_account_id.id}
-		return {'value': value}
-	
-kg_bank_statement()
+	def file_import(self,cr,uid,ids,context=None):
+		rec = self.browse(cr,uid,ids[0])
+		my_string = str(rec.filename)
+		print my_string.split(".",1)[1]
+		if rec.line_id:
+			cr.execute('''delete from ch_bank_statement_import where header_id=%s'''%(rec.id))
+		else:
+			pass
+		try:
+			if my_string.split(".",1)[1] == 'csv':
+				attachments = []
+				attachments.append(base64.b64decode(rec.file_data))
+				dd = [i.split('\n') for i in attachments]
+				ee = [i.split(';') for i in dd[0]]
+				num=0
+				for num in range(1,(len(ee)-1)):
+					cr.execute('''insert into ch_bank_statement_import (header_id,trans_date,ref_no,debit,credit,narration) values ('%s','%s','%s','%s','%s','%s')'''%(rec.id,ee[num][0],ee[num][1],ee[num][2],ee[num][3],ee[num][4]))
+					num = num +1
+			else:
+				bin_file = rec.file_data
+				f = open("/OpenERP/Sam_Turbo/openerp-foundry/openerp-server/openerp/addons/kg_bank_statement_import/temp_bank_state.xls", "wb")
+				f.write(bin_file.decode('base64'))
+				f.close()
+				wb = open_workbook('/OpenERP/Sam_Turbo/openerp-foundry/openerp-server/openerp/addons/kg_bank_statement_import/temp_bank_state.xls')
+				for s in wb.sheets():
+					values = []
+					for row in range(1,s.nrows):
+						col_value = []
+						for col in range(s.ncols):
+							value  = (s.cell(row,col).value)
+							try : value = str(int(value))
+							except : pass
+							col_value.append(value)
+						values.append(col_value)
+				if values:
+					for row in values:
+						cr.execute('''insert into ch_bank_statement_import (header_id,trans_date,ref_no,debit,credit,narration) values ('%s','%s','%s','%s','%s','%s')'''%(rec.id,str(row[0]),str(row[1]),row[2],row[3],str(row[4])))
+		except:
+			raise osv.except_osv(_('Warning!'),
+				_('Incorrect File Type !!'))
+		return True
 
-class ch_bank_statement(osv.osv):
+kg_bank_statement_import()
+
+class ch_bank_statement_import(osv.osv):
 	
-	_name = "ch.bank.statement"
-	_description = "Bank Statement Line"
-	
+	_name = "ch.bank.statement.import"
+	_description = "Bank Statement Import Line"
+	_order='trans_date'
 	_columns = {
 
-	'header_id':fields.many2one('kg.bank.statement','Header id'),
-	'cheque_no':fields.char('Cheque No/Ref No'),
+	'header_id':fields.many2one('kg.bank.statement.import','Header id'),
+	'trans_date':fields.date('Transaction Date'),
+	'ref_no':fields.char('Ref No'),
 	'debit':fields.float('Debit'),
 	'credit':fields.float('Credit'),
 	'narration':fields.text('Narration')
 	
 	}
 	
-ch_bank_statement()	
+ch_bank_statement_import()	
