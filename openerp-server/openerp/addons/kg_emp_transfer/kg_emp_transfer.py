13cfca50fe50fdf386f8130786439aa58e26428f
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-03-30 11:40:22 +0530

    General term master changes done by Thangaraj

---
+++ openerp-server/openerp/addons/kg_emp_transfer/kg_emp_transfer.py
@@ -0,0 +1,216 @@
+from openerp import tools
+from openerp.osv import osv, fields
+from openerp.tools.translate import _
+import time
+import openerp.addons.decimal_precision as dp
+from datetime import datetime
+import datetime
+import re
+import math
+from datetime import date, datetime, timedelta
+import calendar
+dt_time = time.strftime('%m/%d/%Y %H:%M:%S')
+today = date.today()
+
+class kg_emp_transfer(osv.osv):
+
+	_name = "kg.emp.transfer"
+	_description = "Employee Transfer"
+	
+	### Version 0.1
+	
+	def _get_modify(self, cr, uid, ids, field_name, arg,  context=None):
+		res={}		
+		if field_name == 'modify':
+			for h in self.browse(cr, uid, ids, context=None):
+				res[h.id] = 'no'
+				if h.state == 'approved':
+					cr.execute(""" select * from 
+					(SELECT tc.table_schema, tc.constraint_name, tc.table_name, kcu.column_name, ccu.table_name
+					AS foreign_table_name, ccu.column_name AS foreign_column_name
+					FROM information_schema.table_constraints tc
+					JOIN information_schema.key_column_usage kcu ON tc.constraint_name = kcu.constraint_name
+					JOIN information_schema.constraint_column_usage ccu ON ccu.constraint_name = tc.constraint_name
+					WHERE constraint_type = 'FOREIGN KEY'
+					AND ccu.table_name='%s')
+					as sam  """ %('kg_emp_transfer'))
+					data = cr.dictfetchall()	
+					if data:
+						for var in data:
+							data = var
+							chk_sql = 'Select COALESCE(count(*),0) as cnt from '+str(data['table_name'])+' where '+data['column_name']+' = '+str(ids[0])							
+							cr.execute(chk_sql)			
+							out_data = cr.dictfetchone()
+							if out_data:								
+								if out_data['cnt'] > 0:
+									res[h.id] = 'no'
+									return res
+								else:
+									res[h.id] = 'yes'
+				else:
+					res[h.id] = 'no'	
+		return res	
+		
+		### Version 0.2
+		
+		
+	
+	_columns = {
+	
+		## Basic Info
+			
+		'state': fields.selection([('draft','Draft'),('confirmed','WFA'),('approved','Approved'),('reject','Rejected'),('cancel','Cancelled')],'Status', readonly=True),
+		'notes': fields.text('Notes'),
+		'remark': fields.text('Approve/Reject'),
+		'cancel_remark': fields.text('Cancel'),
+		'modify': fields.function(_get_modify, string='Modify', method=True, type='char', size=10),		
+		'entry_mode': fields.selection([('auto','Auto'),('manual','Manual')],'Entry Mode', readonly=True),
+
+		## Entry Info
+		
+		'company_id': fields.many2one('res.company', 'Company Name',readonly=True),
+		'active': fields.boolean('Active'),
+		'crt_date': fields.datetime('Creation Date',readonly=True),
+		'user_id': fields.many2one('res.users', 'Created By', readonly=True),
+		'confirm_date': fields.datetime('Confirmed Date', readonly=True),
+		'confirm_user_id': fields.many2one('res.users', 'Confirmed By', readonly=True),
+		'ap_rej_date': fields.datetime('Approved/Reject Date', readonly=True),
+		'ap_rej_user_id': fields.many2one('res.users', 'Approved/Reject By', readonly=True),	
+		'cancel_date': fields.datetime('Cancelled Date', readonly=True),
+		'cancel_user_id': fields.many2one('res.users', 'Cancelled By', readonly=True),
+		'update_date': fields.datetime('Last Updated Date', readonly=True),
+		'update_user_id': fields.many2one('res.users', 'Last Updated By', readonly=True),
+		
+		## Module Requirement Info
+		
+		'employee_id':fields.many2one('hr.employee','Employee'),
+		'emp_code':fields.char('Code'),
+		'emp_categ_id':fields.many2one('kg.employee.category','Category'),
+		'division_id':fields.many2one('kg.division.master','Division'),
+		'from_division_id':fields.many2one('kg.division.master','From Division'),
+		'to_division_id':fields.many2one('kg.division.master','To Division'),
+		'from_date':fields.date('From Date'),
+		'to_date':fields.date('To Date'),
+		
+				
+	}
+	
+	_defaults = {
+	
+		'company_id': lambda self,cr,uid,c: self.pool.get('res.company')._company_default_get(cr, uid, 'kg.emp.transfer', context=c),
+		'active': True,
+		'state': 'draft',
+		'user_id': lambda obj, cr, uid, context: uid,
+		'crt_date':lambda * a: time.strftime('%Y-%m-%d %H:%M:%S'),
+		'modify': 'no',
+		'entry_mode': 'manual',
+		
+	}
+	
+	####Validations####
+	
+	def  _validations (self,cr,uid,ids,context=None):
+		rec = self.browse(cr,uid,ids[0])
+		if rec.from_division_id == rec.to_division_id:
+			raise osv.except_osv(_('Warning !!'),
+				_('From and To Division should not be same !!'))
+			return False
+		return True
+						
+	_constraints = [
+
+		(_validations, 'validations', [' ']),		
+		
+	]
+	
+	## Basic Needs	
+	
+	def entry_cancel(self,cr,uid,ids,context=None):
+		
+		rec = self.browse(cr,uid,ids[0])
+		
+		if rec.state == 'approved':
+						
+			if rec.cancel_remark:
+				self.write(cr, uid, ids, {'state': 'cancel','cancel_user_id': uid, 'cancel_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+			else:
+				raise osv.except_osv(_('Cancel remark is must !!'),
+					_('Enter the remarks in Cancel remarks field !!'))
+		else:
+			pass
+			
+		return True
+
+	def entry_confirm(self,cr,uid,ids,context=None):
+		
+		rec = self.browse(cr,uid,ids[0])
+		
+		if rec.state == 'draft':
+			self.write(cr, uid, ids, {'state': 'confirmed','confirm_user_id': uid, 'confirm_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+		
+		else:
+			pass
+			
+		return True
+		
+	def entry_draft(self,cr,uid,ids,context=None):
+		
+		rec = self.browse(cr,uid,ids[0])
+		
+		if rec.state == 'approved':			
+			self.write(cr, uid, ids, {'state': 'draft'})
+		else:
+			pass
+			
+		return True
+
+	def entry_approve(self,cr,uid,ids,context=None):
+		rec = self.browse(cr,uid,ids[0])
+		if rec.state == 'confirmed':
+			emp_rec = self.pool.get('hr.employee').browse(cr,uid,rec.employee_id.id)
+			print "employee division",emp_rec.division_id.id
+			print "emp_rec.idemp_rec.idemp_rec.id",emp_rec.id
+			self.pool.get('hr.employee').write(cr,uid,emp_rec.id,{'division_id':rec.to_division_id.id})
+			self.write(cr, uid, ids, {'state': 'approved','ap_rej_user_id': uid, 'ap_rej_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+		else:
+			pass
+		return True
+
+	def entry_reject(self,cr,uid,ids,context=None):
+		rec = self.browse(cr,uid,ids[0])
+		if rec.state == 'confirmed':
+			if rec.remark:
+				self.write(cr, uid, ids, {'state': 'reject','ap_rej_user_id': uid, 'ap_rej_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+			else:
+				raise osv.except_osv(_('Rejection remark is must !!'),
+					_('Enter the remarks in rejection remark field !!'))
+		else:
+			pass
+		return True
+		
+	def unlink(self,cr,uid,ids,context=None):
+		unlink_ids = []		
+		for rec in self.browse(cr,uid,ids):	
+			if rec.state not in ('draft','cancel'):				
+				raise osv.except_osv(_('Warning!'),
+						_('You can not delete this entry !!'))
+			else:
+				unlink_ids.append(rec.id)
+		return osv.osv.unlink(self, cr, uid, unlink_ids, context=context)
+		
+		
+	def write(self, cr, uid, ids, vals, context=None):
+		vals.update({'update_date': time.strftime('%Y-%m-%d %H:%M:%S'),'update_user_id':uid})
+		return super(kg_emp_transfer, self).write(cr, uid, ids, vals, context)	
+	
+	
+	## Module Requirement
+	
+	def on_change_emp(self,cr,uid,ids,employee_id,emp_code,emp_categ_id,division_id,from_division_idcontext=None):
+		emp_rec = self.pool.get('hr.employee').browse(cr,uid,employee_id)
+		value = {'emp_code':emp_rec.code,'emp_categ_id':emp_rec.emp_categ_id.id,'division_id':emp_rec.division_id.id,'from_division_id':emp_rec.division_id.id}
+		return {'value': value}
+	
+kg_emp_transfer()
+
+
