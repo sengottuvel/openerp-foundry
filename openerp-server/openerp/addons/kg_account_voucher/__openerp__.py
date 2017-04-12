4eaee4b0ac07bba02bce821c881a490e96a3570a
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-04-12 19:06:50 +0530

    account Voucher For Karthikeyan 11-12-2017

--- openerp-server/openerp/addons/kg_account_voucher/__openerp__.py
+++ openerp-server/openerp/addons/kg_account_voucher/__openerp__.py
@@ -11,7 +11,7 @@
     'category': 'BASE',        
     'depends' : ['base','kg_sale_invoice','kg_customer_advance','kg_division_master','kg_subcontract_invoice','kg_foundry_invoice'],
     'data': ['kg_account_voucher_view.xml',
-				'jasper_report.xml'],
+				],
     'css': ['static/src/css/state.css'], 
     'auto_install': False,
     'installable': True,
