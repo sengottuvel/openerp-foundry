d23765fb869a058f5a125bcc93844d4615614fee
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-06-22 15:47:37 +0530

    Scheduler changes done by Thangaraj

---
+++ schedulers/scheduler_excel_deletion.py
@@ -0,0 +1,15 @@
+import xmlrpclib
+username = 'admin' #the user
+pwd = 'admin'      #the password of the user
+dbname = 'sam_phaseI'    #the database
+# Server Connectivity
+
+sock_common = xmlrpclib.ServerProxy ('http://localhost:8070/xmlrpc/common')
+
+uid = sock_common.login(dbname, username, pwd)
+
+sock = xmlrpclib.ServerProxy('http://localhost:8070/xmlrpc/object')
+
+## Scheduler List#
+#~ sock.execute(dbname, uid, pwd, 'kg.scheduler', 'weekly_excel_report_deletion')
+sock.execute(dbname, uid, pwd, 'kg.scheduler', 'auto_primecost_delete')
