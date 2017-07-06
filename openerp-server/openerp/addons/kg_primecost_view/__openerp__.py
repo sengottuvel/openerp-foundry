1e946b2d9ed92ea8dc3e3cc94d27f3d6edddbfee
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-07-06 11:45:32 +0530

    Primecost view changes done by Thangaraj

--- openerp-server/openerp/addons/kg_primecost_view/__openerp__.py
+++ openerp-server/openerp/addons/kg_primecost_view/__openerp__.py
@@ -8,7 +8,7 @@
     'name': 'Primecost View',
     'version': '0.1',
     'author': 'Thangaraj',    
-    'depends' : ['base','kg_moc_construction','kg_pumpmodel_master','kg_position_number','kg_pattern_master','kg_moc_master','kg_machine_shop','kg_po_masters'],
+    'depends' : ['base','kg_moc_construction','kg_pumpmodel_master','kg_position_number','kg_pattern_master','kg_moc_master','kg_machine_shop','kg_po_masters','kg_bom'],
     'data': [
     
 			'kg_primecost_view.xml',
