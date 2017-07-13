c148d1963bb227dd4eda270123f278a4157d7d69
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-07-13 10:35:43 +0530

    CD Export copy enabled by Thangaraj

--- openerp-server/openerp/addons/kg_crm_offer/report/__init__.py
+++ openerp-server/openerp/addons/kg_crm_offer/report/__init__.py
@@ -1,7 +1,5 @@
 import onscreen_cd_report
 import onscreen_cd_export_report
-import onscreen_annexure_1_report
+import onscreen_offer_copy_report
 import onscreen_spare_regular_report
 import onscreen_spare_budgetary_report
-import onscreen_dealer_wo_ack_report
-import onscreen_customer_wo_ack_report
