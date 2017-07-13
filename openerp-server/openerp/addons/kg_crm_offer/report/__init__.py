e835acc6d8742a3b2d7eee13bbfdafc7e1a3e7da
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-07-13 11:39:08 +0530

    Dealer and Customer reports enabled in CRM Offer

--- openerp-server/openerp/addons/kg_crm_offer/report/__init__.py
+++ openerp-server/openerp/addons/kg_crm_offer/report/__init__.py
@@ -3,3 +3,5 @@ import onscreen_cd_export_report
 import onscreen_offer_copy_report
 import onscreen_spare_regular_report
 import onscreen_spare_budgetary_report
+import onscreen_dealer_wo_ack_report
+import onscreen_customer_wo_ack_report
---
+++ openerp-server/openerp/addons/kg_crm_offer/report/onscreen_dealer_wo_ack_report.py
@@ -0,0 +1,40 @@
+from kg_purchase_order import JasperDataParser
+from jasper_reports import jasper_report
+import pooler
+import time, datetime
+import base64
+import netsvc
+from osv import osv, fields
+from tools.translate import _
+from osv.orm import browse_record, browse_null
+import os
+
+class onscreen_dealer_wo_ack_report(JasperDataParser.JasperDataParser):
+	def __init__(self, cr, uid, ids, data, context):
+		
+		super(onscreen_dealer_wo_ack_report, self).__init__(cr, uid, ids, data, context)
+	
+	def generate_data_source(self, cr, uid, ids, data, context):
+		return 'records'
+	
+	def generate_ids(self, cr, uid, ids, data, context):
+		return {}
+	
+	def generate_properties(self, cr, uid, ids, data, context):
+		return {}
+	
+	def generate_parameters(self, cr, uid, ids, data, context):
+		val={}	
+		user_rec = self.pool.get('res.users').browse(cr,uid,uid)
+			
+		val['offer_id'] = ids[0]
+		val['printed_by'] = user_rec.name
+		print "val....................", val
+		
+		return val
+	
+	def generate_records(self, cr, uid, ids, data, context):
+		pool= pooler.get_pool(cr.dbname)		
+		return {}
+	
+jasper_report.report_jasper('report.onscreen_dealer_wo_ack_report', 'kg.crm.offer', parser=onscreen_dealer_wo_ack_report)
