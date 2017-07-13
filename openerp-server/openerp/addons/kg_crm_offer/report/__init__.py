73af984043b8d74c56cd49f934c92440ad7dd690
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-07-13 10:31:26 +0530

    CD Export copy enabled by Thangaraj

--- openerp-server/openerp/addons/kg_crm_offer/report/__init__.py
+++ openerp-server/openerp/addons/kg_crm_offer/report/__init__.py
@@ -1,4 +1,8 @@
 import onscreen_cd_report
-import onscreen_offer_copy_report
+import onscreen_cd_export_report
+import onscreen_offer_export_copy_report
+import onscreen_annexure_1_report
 import onscreen_spare_regular_report
 import onscreen_spare_budgetary_report
+import onscreen_dealer_wo_ack_report
+import onscreen_customer_wo_ack_report
---
+++ openerp-server/openerp/addons/kg_crm_offer/report/onscreen_cd_export_report.py
@@ -0,0 +1,37 @@
+from kg_purchase_order import JasperDataParser
+from jasper_reports import jasper_report
+import pooler
+import time, datetime
+import base64
+import netsvc
+from osv import osv, fields
+from tools.translate import _
+from osv.orm import browse_record, browse_null
+import os
+
+class onscreen_cd_export_report(JasperDataParser.JasperDataParser):
+	
+	def __init__(self, cr, uid, ids, data, context):
+		super(onscreen_cd_export_report, self).__init__(cr, uid, ids, data, context)
+	
+	def generate_data_source(self, cr, uid, ids, data, context):
+		return 'records'
+	
+	def generate_ids(self, cr, uid, ids, data, context):
+		return {}
+	
+	def generate_properties(self, cr, uid, ids, data, context):
+		return {}
+	
+	def generate_parameters(self, cr, uid, ids, data, context):
+		val={}		
+		val['offer_id'] = ids[0]
+		print "val....................", val
+		
+		return val
+	
+	def generate_records(self, cr, uid, ids, data, context):
+		pool= pooler.get_pool(cr.dbname)		
+		return {}
+	
+jasper_report.report_jasper('report.onscreen_cd_export_report', 'kg.crm.offer', parser=onscreen_cd_export_report)
