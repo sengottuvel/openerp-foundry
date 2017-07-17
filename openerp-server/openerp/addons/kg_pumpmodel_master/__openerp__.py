71d8bcf571d563eab5d08b27e1bbd198e808d359
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-07-17 19:43:40 +0530

    HSN Code Master added by Karthikeyan

--- openerp-server/openerp/addons/kg_pumpmodel_master/__openerp__.py
+++ openerp-server/openerp/addons/kg_pumpmodel_master/__openerp__.py
@@ -9,7 +9,7 @@
     'version': '0.1',
     'author': 'karthikeyan',
     'category': 'BASE',        
-    'depends' : ['base','kg_pump_category','kg_pumpseries_master','kg_hsn_master'],
+    'depends' : ['base','kg_pump_category','kg_pumpseries_master'],
     'data': ['kg_pumpmodel_master_view.xml'],
     'css': ['static/src/css/state.css'], 
     'auto_install': False,
