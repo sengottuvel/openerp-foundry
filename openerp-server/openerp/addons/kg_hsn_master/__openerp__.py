2fddef9825d17e38cd4e4283686576ba61cde207
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-07-17 19:29:59 +0530

    HSN Code Master added by Karthikeyan

---
+++ openerp-server/openerp/addons/kg_hsn_master/__openerp__.py
@@ -0,0 +1,18 @@
+##############################################################################
+#
+#   This is New Customized Pump Model Master
+#
+##############################################################################
+
+{
+    'name': 'HSN Code Master',
+    'version': '0.1',
+    'author': 'karthikeyan',
+    'category': 'BASE',        
+    'depends' : ['base','account'],
+    'data': ['kg_hsn_master_view.xml'],
+    'css': ['static/src/css/state.css'], 
+    'auto_install': False,
+    'installable': True,
+}
+
---
+++ openerp-server/openerp/addons/kg_hsn_master/kg_hsn_master.py
@@ -0,0 +1,161 @@
+from openerp import tools
+from openerp.osv import osv, fields
+from openerp.tools.translate import _
+import time
+import openerp.addons.decimal_precision as dp
+from datetime import datetime
+import re
+import math
+dt_time = time.strftime('%m/%d/%Y %H:%M:%S')
+
+class kg_hsn_master(osv.osv):
+
+	_name = "kg.hsn.master"
+	_description = "HSN Code Master"
+	
+	def _get_modify(self, cr, uid, ids, field_name, arg,  context=None):
+		res={}		
+		if field_name == 'modify':
+			for h in self.browse(cr, uid, ids, context=None):
+				res[h.id] = 'no'
+				if h.state == 'approved':
+					cr.execute(""" select * from 
+					(SELECT tc.table_schema, tc.constraint_name, tc.table_name, kcu.column_name, ccu.table_name
+					AS foreign_table_name, ccu.column_name AS foreign_column_name
+					FROM information_schema.table_constraints tc
+					JOIN information_schema.key_column_usage kcu ON tc.constraint_name = kcu.constraint_name
+					JOIN information_schema.constraint_column_usage ccu ON ccu.constraint_name = tc.constraint_name
+					WHERE constraint_type = 'FOREIGN KEY'
+					AND ccu.table_name='%s')
+					as sam  """ %('kg_hsn_master'))
+					data = cr.dictfetchall()	
+					if data:
+						for var in data:
+							data = var
+							chk_sql = 'Select COALESCE(count(*),0) as cnt from '+str(data['table_name'])+' where '+data['column_name']+' = '+str(ids[0])							
+							cr.execute(chk_sql)			
+							out_data = cr.dictfetchone()
+							if out_data:								
+								if out_data['cnt'] > 0:
+									res[h.id] = 'no'
+									return res
+								else:
+									res[h.id] = 'yes'
+				else:
+					res[h.id] = 'no'	
+		return res	
+	
+	_columns = {
+	
+	
+			
+		'name': fields.integer('HSN Code', size=8, required=True, select=True),
+		'company_id': fields.many2one('res.company', 'Company Name',readonly=True),		
+		'active': fields.boolean('Active'),
+		'state': fields.selection([('draft','Draft'),('confirmed','WFA'),('approved','Approved'),('reject','Rejected'),('cancel','Cancelled')],'Status', readonly=True),
+		'notes': fields.text('Notes'),
+		'remark': fields.text('Approve/Reject'),
+		'cancel_remark': fields.text('Cancel'),
+		'modify': fields.function(_get_modify, string='Modify', method=True, type='char', size=10),		
+		
+		### Entry Info ###
+		'crt_date': fields.datetime('Creation Date',readonly=True),
+		'user_id': fields.many2one('res.users', 'Created By', readonly=True),
+		'confirm_date': fields.datetime('Confirmed Date', readonly=True),
+		'confirm_user_id': fields.many2one('res.users', 'Confirmed By', readonly=True),
+		'ap_rej_date': fields.datetime('Approved/Reject Date', readonly=True),
+		'ap_rej_user_id': fields.many2one('res.users', 'Approved/Reject By', readonly=True),	
+		'cancel_date': fields.datetime('Cancelled Date', readonly=True),
+		'cancel_user_id': fields.many2one('res.users', 'Cancelled By', readonly=True),
+		'update_date': fields.datetime('Last Updated Date', readonly=True),
+		'update_user_id': fields.many2one('res.users', 'Last Updated By', readonly=True),
+		
+		'cgst_id': fields.many2one('account.tax', 'CGST', required=True),
+		'sgst_id': fields.many2one('account.tax', 'SGST', required=True),
+		'igst_id': fields.many2one('account.tax', 'IGST', required=True),
+				
+	}
+	
+	_defaults = {
+	
+		'company_id': lambda self,cr,uid,c: self.pool.get('res.company')._company_default_get(cr, uid, 'kg.hsn.master', context=c),
+		'active': True,
+		'state': 'draft',
+		'user_id': lambda obj, cr, uid, context: uid,
+		'crt_date':lambda * a: time.strftime('%Y-%m-%d %H:%M:%S'),
+		'modify': 'no',
+		
+	}
+	
+	_sql_constraints = [
+	
+		('name', 'unique(name)', 'HSN Code must be unique !!'),
+		
+	]	
+		
+	def _name_validate(self, cr, uid,ids, context=None):
+		rec = self.browse(cr,uid,ids[0])
+		res = True
+		if rec.name:
+			name = rec.name		
+			cr.execute(""" select name from kg_hsn_master where name  = '%s' """ %(name))
+			data = cr.dictfetchall()			
+			if len(data) > 1:
+				res = False
+			else:
+				res = True				
+		return res
+			
+	
+	def entry_cancel(self,cr,uid,ids,context=None):
+		rec = self.browse(cr,uid,ids[0])
+		if rec.cancel_remark:
+			self.write(cr, uid, ids, {'state': 'cancel','cancel_user_id': uid, 'cancel_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+		else:
+			raise osv.except_osv(_('Cancel remark is must !!'),
+				_('Enter the remarks in Cancel remarks field !!'))
+		return True
+
+	def entry_confirm(self,cr,uid,ids,context=None):
+		self.write(cr, uid, ids, {'state': 'confirmed','confirm_user_id': uid, 'confirm_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+		return True
+	def entry_draft(self,cr,uid,ids,context=None):
+		self.write(cr, uid, ids, {'state': 'draft'})
+		return True
+
+	def entry_approve(self,cr,uid,ids,context=None):
+		self.write(cr, uid, ids, {'state': 'approved','ap_rej_user_id': uid, 'ap_rej_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+		return True
+
+	def entry_reject(self,cr,uid,ids,context=None):
+		rec = self.browse(cr,uid,ids[0])
+		if rec.remark:
+			self.write(cr, uid, ids, {'state': 'reject','ap_rej_user_id': uid, 'ap_rej_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+		else:
+			raise osv.except_osv(_('Rejection remark is must !!'),
+				_('Enter the remarks in rejection remark field !!'))
+		return True
+		
+	def unlink(self,cr,uid,ids,context=None):
+		unlink_ids = []		
+		for rec in self.browse(cr,uid,ids):	
+			if rec.state not in ('draft','cancel'):				
+				raise osv.except_osv(_('Warning!'),
+						_('You can not delete this entry !!'))
+			else:
+				unlink_ids.append(rec.id)
+		return osv.osv.unlink(self, cr, uid, unlink_ids, context=context)
+		
+	def write(self, cr, uid, ids, vals, context=None):
+		vals.update({'update_date': time.strftime('%Y-%m-%d %H:%M:%S'),'update_user_id':uid})
+		return super(kg_hsn_master, self).write(cr, uid, ids, vals, context)
+		
+	
+	_constraints = [
+		
+		(_name_validate, 'HSN name must be unique !!', ['name']),		
+		
+		
+	]
+	
+kg_hsn_master()
