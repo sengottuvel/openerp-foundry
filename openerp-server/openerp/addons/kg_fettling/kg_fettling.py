2e2de55c7172476fd868aa51bdb2600a555c90c5
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-07-20 14:13:21 +0530

    Fettling changes done by sangeetha

--- openerp-server/openerp/addons/kg_fettling/kg_fettling.py
+++ openerp-server/openerp/addons/kg_fettling/kg_fettling.py
@@ -572,7 +572,7 @@ class kg_fettling(osv.osv):
 			print "entry_rec.entry_rec.order_no",entry_rec.order_no
 			print "entry_rec.entry_rec.order_line_id",entry_rec.order_line_id
 			print "entry_rec.entry_rec.entry_rec.pattern_name",entry_rec.pattern_name
-			print "entry_rec.order_bomline_id.position_id",entry_rec.order_bomline_id.position_id
+			print "entry_rec.order_bomline_id.position_id------------------",entry_rec.order_bomline_id.position_id
 			ms_vals = {
 			'name': ms_name[0],
 			'location':entry_rec.location,
