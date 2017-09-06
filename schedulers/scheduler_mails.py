806a2912c481b09a9032192116416f917dcf76b3
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-09-06 11:54:36 +0530

    Scheduler changes done by Thangaraj

--- schedulers/scheduler_mails.py
+++ schedulers/scheduler_mails.py
@@ -11,9 +11,13 @@ uid = sock_common.login(dbname, username, pwd)
 sock = xmlrpclib.ServerProxy('http://localhost:8069/xmlrpc/object')
 
 ## Scheduler List#
-sock.execute(dbname, uid, pwd, 'kg.scheduler', 'planning_vs_production_register_scheduler_mail')
+#~ sock.execute(dbname, uid, pwd, 'kg.scheduler', 'planning_vs_production_register_scheduler_mail')
 
-sock.execute(dbname, uid, pwd, 'kg.scheduler', 'daily_stock_statement_scheduler_mail')
+#~ sock.execute(dbname, uid, pwd, 'kg.scheduler', 'daily_stock_statement_scheduler_mail')
 
-sock.execute(dbname, uid, pwd, 'kg.scheduler', 'transaction_summary_list_scheduler_mail')
+#~ sock.execute(dbname, uid, pwd, 'kg.scheduler', 'transaction_summary_list_scheduler_mail')
+
+sock.execute(dbname, uid, pwd, 'kg.scheduler', 'userwise_summary_list_scheduler_mail')
+
+sock.execute(dbname, uid, pwd, 'kg.scheduler', 'daily_approved_po_grn_summary')
 
