1b50431f6fc1039aeb90c9e953ad52233f3594dc
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-09-06 11:46:45 +0530

    Scheduler changes done by Thangaraj

--- openerp-server/openerp/addons/kg_scheduler/kg_scheduler.py
+++ openerp-server/openerp/addons/kg_scheduler/kg_scheduler.py
@@ -156,6 +156,41 @@ class kg_scheduler(osv.osv):
 				pass
 			return True
 	
+	def daily_approved_po_grn_summary(self,cr,uid,ids=0,context = None):		
+		
+		cr.execute("""select sch_approved_po_grn('approved po and grn')""")
+		data = cr.fetchall();
+		print "data<<<<<<<<<", data
+		
+		if data[0][0] is None:
+			return False
+		if data[0][0] is not None:	
+			maildet = (str(data[0])).rsplit('~');
+			cont = data[0][0].partition('UNWANTED.')		
+			email_from = maildet[1]	
+			if maildet[2]:	
+				email_to = [maildet[2]]
+			else:
+				email_to = ['']			
+			if maildet[3]:
+				email_cc = [maildet[3]]	
+			else:
+				email_cc = ['']		
+			ir_mail_server = self.pool.get('ir.mail_server')
+			if maildet[4] != '':
+				msg = ir_mail_server.build_email(
+					email_from = email_from,
+					email_to = email_to,
+					subject = maildet[4],
+					body = cont[0],
+					email_cc = email_cc,
+					object_id = ids and ('%s-%s' % (ids, 'kg.mail.settings')),
+					subtype = 'html',
+					subtype_alternative = 'plain')
+				res = ir_mail_server.send_email(cr, uid, msg,mail_server_id=1, context=context)
+			else:
+				pass
+			return True
 	
 	## Fiscal Year Opening Stock Creation Process Start
 	
