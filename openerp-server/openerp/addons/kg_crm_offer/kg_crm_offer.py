c522883e9f283a4245908991618f227bddd170b6
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-09-11 15:23:26 +0530

    PO Access changes done by Thangaraj

--- openerp-server/openerp/addons/kg_crm_offer/kg_crm_offer.py
+++ openerp-server/openerp/addons/kg_crm_offer/kg_crm_offer.py
@@ -572,7 +572,7 @@ class kg_crm_offer(osv.osv):
 																  'offer_no': entry.name,
 																  'project_name': entry.prj_name,
 																  'division_id': entry.division_id.id,
-																  'delaer_id': entry.delaer_id.id,
+																  'dealer_id': entry.dealer_id.id,
 																  #~ 'location': entry.location,
 																  'entry_mode': 'auto',
 																  'partner_id': entry.customer_id.id,
