ad8f9f81ac2b8fa73b017843d0f110ebc3ba8825
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-09-12 15:39:15 +0530

    Issue register changes done by Thangaraj

--- openerp-server/openerp/addons/kg_reports/warehouse/report/kg_dep_issue.py
+++ openerp-server/openerp/addons/kg_reports/warehouse/report/kg_dep_issue.py
@@ -14,21 +14,22 @@ import re
 from HTMLParser import HTMLParser
 
 class kg_dep_issue(JasperDataParser.JasperDataParser):
+	
 	def __init__(self, cr, uid, ids, data, context):
 		super(kg_dep_issue, self).__init__(cr, uid, ids, data, context)
-
+	
 	def generate_data_source(self, cr, uid, ids, data, context):
 		return 'records'
-
+	
 	def generate_ids(self, cr, uid, ids, data, context):
 		return {}
-
+	
 	def generate_properties(self, cr, uid, ids, data, context):
 		return {}
-		
+	
 	def generate_parameters(self, cr, uid, ids, data, context):
 		val={}
-	
+		
 		user_obj = self.pool.get('res.users').search(self.cr, self.uid, [('id','=',uid)])
 		user_id = self.pool.get('res.users').browse(self.cr,self.uid,user_obj[0])
 		user_rec = user_id.login
@@ -55,10 +56,9 @@ class kg_dep_issue(JasperDataParser.JasperDataParser):
 		altered_time=data['form']['print_date']
 		val['print_date'] = crt_time
 		return val
-
-
+	
 	def generate_records(self, cr, uid, ids, data, context):
 		pool= pooler.get_pool(cr.dbname)
 		return {}
-
+	
 jasper_report.report_jasper('report.jasper_kg_dep_issue', 'kg.department.issue', parser=kg_dep_issue)
--- openerp-server/openerp/addons/kg_reports/warehouse/wizard/dep_issue_register_wizard.py
+++ openerp-server/openerp/addons/kg_reports/warehouse/wizard/dep_issue_register_wizard.py
@@ -8,21 +8,24 @@ from osv.orm import browse_record, browse_null
 from tools.translate import _
 
 class dep_issue_register_wizard(osv.osv_memory):
+	
 	_name = "dep.issue.register.wizard"
 	_description = "Issue Register Wiz"
 	
 	_columns = {
-	
-				'fis_year': fields.many2one('account.fiscalyear','Fiscal year',readonly=True),
-				'user_id': fields.many2one('res.users', 'User ID'),
-				'company_id': fields.many2one('res.company', 'Company Name',required=True),
-				'from_date': fields.date('From Date'),
-				'to_date': fields.date('To Date'),
-				'dep_id':fields.many2many('kg.depmaster','dep_issue_register2','wiz_id','dep_id','Department Name',domain="[('state','in',('draft','confirm','approved'))]"),
-				'product':fields.many2many('product.product','dep_issue_product2','product_wiz_id','product_dep_id','Product',domain="[('state','in',('draft','confirm','approved'))]"),
+				
+				'fis_year'	  : fields.many2one('account.fiscalyear','Fiscal year',readonly=True),
+				'user_id'	  : fields.many2one('res.users', 'User ID'),
+				'company_id'  : fields.many2one('res.company', 'Company Name',required=True),
+				'from_date'   : fields.date('From Date'),
+				'to_date'	  : fields.date('To Date'),
+				'dep_id'	  : fields.many2many('kg.depmaster','dep_issue_register2','wiz_id','dep_id','Department',domain="[('state','in',('draft','confirm','approved'))]"),
+				'product' 	  : fields.many2many('product.product','dep_issue_product2','product_wiz_id','product_dep_id','Product',domain="[('state','in',('draft','confirm','approved'))]"),
+				'moc_id' 	  : fields.many2many('kg.moc.master','dep_issue_wiz','wiz_id','moc_id','MOC'),
 				'issue_status': fields.selection([('approved', 'Approved'),('cancelled','Cancelled')], "Status"),
-				'filter': fields.selection([('filter_no', 'No Filters'), ('filter_date', 'Date')], "Filter by"),
-				'print_date': fields.datetime('Creation Date', readonly=True),
+				'filter'	  : fields.selection([('filter_no', 'No Filters'), ('filter_date', 'Date')], "Filter by"),
+				'print_date'  : fields.datetime('Creation Date', readonly=True),
+				
 			}
 	
 	def _get_from_date(self, cr, uid, context=None):
@@ -32,7 +35,6 @@ class dep_issue_register_wizard(osv.osv_memory):
 		from_date = fis_id.date_start
 		d2 = datetime.strptime(from_date,'%Y-%m-%d')
 		res = d2.strftime('%Y-%m-%d')
-		
 		return res
 	
 	def _get_fis(self, cr, uid, context=None):
@@ -41,59 +43,45 @@ class dep_issue_register_wizard(osv.osv_memory):
 		fis_id = self.pool.get('account.fiscalyear').browse(cr,uid,fis_obj[0])
 		fisyear_id = fis_id.id
 		return fisyear_id
-		
-		
-						
+	
 	_defaults = {
 				
-				'user_id': lambda self, cr, uid, c: self.pool.get('res.users').browse(cr, uid, uid, c).id,
-				'company_id':1,
-				'from_date': lambda * a: time.strftime('%Y-%m-%d'),
-				'to_date': lambda * a: time.strftime('%Y-%m-%d'),
-				'fis_year': _get_fis,
-				'filter':'filter_date',
+				'user_id'	: lambda self, cr, uid, c: self.pool.get('res.users').browse(cr, uid, uid, c).id,
+				'company_id': 1,
+				'from_date' : lambda * a: time.strftime('%Y-%m-%d'),
+				'to_date'	: lambda * a: time.strftime('%Y-%m-%d'),
+				'fis_year'	: _get_fis,
+				'filter'	: 'filter_date',
 				'print_date': lambda * a: time.strftime('%Y-%m-%d %H:%M:%S'),
 				
 				}
-		
+	
 	def create_report(self, cr, uid, ids, context={}):
 		data = self.read(cr,uid,ids,)[-1]
+		#~ print data,' create_report('
 		return {
-			'type'		 : 'ir.actions.report.xml',
-			'report_name'   : 'jasper_kg_dep_issue',
-			'datas': {
-					'model':'dep.issue.register.wizard',
-					'id': context.get('active_ids') and context.get('active_ids')[0] or False,
-					'ids': context.get('active_ids') and context.get('active_ids') or [],
-					'report_type':'pdf',
-					'form':data
-				},
+			'type'		  : 'ir.actions.report.xml',
+			'report_name' : 'jasper_kg_dep_issue',
+			'datas'		  : {
+							'model'		 : 'dep.issue.register.wizard',
+							'id'		 : context.get('active_ids') and context.get('active_ids')[0] or False,
+							'ids'		 : context.get('active_ids') and context.get('active_ids') or [],
+							'report_type': 'pdf',
+							'form'		 : data
+							},
 			'nodestroy': False
 			}
-			
-	def _date_check(self,cr,uid,ids,context=None):
-		rec=self.browse(cr,uid,ids[0])
-		current_date=time.strftime('%Y-%m-%d')
-		if rec.from_date > current_date or rec.to_date > current_date:
-			raise osv.except_osv(_('Warning!'),
-						_('Future Date are not allowed in Start Date and End Date!!'))
-			return False
-		return True
-		
+	
 	def _enddate_check(self,cr,uid,ids,context=None):
 		rec=self.browse(cr,uid,ids[0])
 		if rec.to_date < rec.from_date:
-			raise osv.except_osv(_('Warning!'),
-						_('End Date is lesser than Start Date!!'))
-			return False
+			raise osv.except_osv(_('Warning!'),_('End Date is lesser than Start Date!!'))
 		return True
-		
-	_constraints = [
 	
-		(_date_check, 'Future Dates are Not Allowed !!!', ['Check Date']),
+	_constraints = [
+		
 		(_enddate_check, 'Future Dates are Not Allowed !!!', ['Check Date']),
-
+		
 	]
 	
 dep_issue_register_wizard()
-
