7ca7a82b5f8ff6237b67e1427a8a38c64c12e57b
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-11-16 15:49:00 +0530

    DMS Changes done by sujith

--- openerp-server/openerp/addons/kg_machinery_master/kg_machinery_master.py
+++ openerp-server/openerp/addons/kg_machinery_master/kg_machinery_master.py
@@ -6,6 +6,7 @@ import openerp.addons.decimal_precision as dp
 from datetime import datetime
 import re
 import math
+import base64
 dt_time = time.strftime('%m/%d/%Y %H:%M:%S')
 
 class kg_machinery_master(osv.osv):
@@ -163,6 +164,26 @@ class kg_machinery_master(osv.osv):
 	def write(self, cr, uid, ids, vals, context=None):
 		vals.update({'update_date': time.strftime('%Y-%m-%d %H:%M:%S'),'update_user_id':uid})
 		return super(kg_machinery_master, self).write(cr, uid, ids, vals, context)
+	
+	def send_to_dms(self,cr,uid,ids,context=None):
+		rec = self.browse(cr,uid,ids[0])
+		res_rec=self.pool.get('res.users').browse(cr,uid,uid)		
+		rec_user = str(res_rec.login)
+		rec_pwd = str(res_rec.password)
+		rec_code = str(rec.name)		
+		encoded_user = base64.b64encode(rec_user)
+		encoded_pwd = base64.b64encode(rec_pwd)
+			
+		url = 'http://192.168.1.7/sam-dms/login.html?xmxyypzr='+encoded_user+'&mxxrqx='+encoded_pwd+'&Machinery='+rec_code
+
+
+		return {
+					  'name'	 : 'Go to website',
+					  'res_model': 'ir.actions.act_url',
+					  'type'	 : 'ir.actions.act_url',
+					  'target'   : 'current',
+					  'url'	  : url
+			   }
 		
 	
 	_constraints = [
