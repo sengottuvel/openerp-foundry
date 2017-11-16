d6650f2e402e8f25f54352f9db3a794cbb0dbe54
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-11-16 17:55:58 +0530

    Users changes done by Karthikeyan

--- openerp-server/openerp/addons/kg_purchase_order/report/onscreen_purchase_order_report.py
+++ openerp-server/openerp/addons/kg_purchase_order/report/onscreen_purchase_order_report.py
@@ -1,25 +1,3 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#	OpenERP, Open Source Management Solution
-#	Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
-#	$Id$
-#
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
-#
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 from kg_purchase_order import JasperDataParser
 from jasper_reports import jasper_report
 import pooler
@@ -47,13 +25,12 @@ class onscreen_purchase_order_report(JasperDataParser.JasperDataParser):
 	
 	def generate_parameters(self, cr, uid, ids, data, context):
 		val={}
-		print "uid....................",uid
 		rec = self.pool.get('purchase.order').browse(cr,uid,ids[0])
-		print"rererere",rec.id
+
 		
 		if rec.state == 'confirmed':
 			if rec.confirmed_by.signature:
-				confirmed_signature = rec.confirmed_by.signature
+				confirmed_signature = rec.confirmed_by.scaned_signature
 				myfile = '/OpenERP/Sam_Turbo/openerp-foundry/openerp-server/openerp/addons/kg_purchase_order/images/Confirmed.jpg'
 				if os.path.isfile(myfile) == True:
 					os.remove(myfile)
@@ -66,7 +43,7 @@ class onscreen_purchase_order_report(JasperDataParser.JasperDataParser):
 				pass
 		elif rec.state == 'verified':
 			if rec.confirmed_by.signature:
-				confirmed_signature = rec.confirmed_by.signature
+				confirmed_signature = rec.confirmed_by.scaned_signature
 				myfile = '/OpenERP/Sam_Turbo/openerp-foundry/openerp-server/openerp/addons/kg_purchase_order/images/Confirmed.jpg'
 				if os.path.isfile(myfile) == True:
 					os.remove(myfile)
@@ -78,7 +55,7 @@ class onscreen_purchase_order_report(JasperDataParser.JasperDataParser):
 			else:
 				pass
 			if rec.verified_by.signature:
-				verified_signature = rec.verified_by.signature
+				verified_signature = rec.verified_by.scaned_signature
 				myfile = '/OpenERP/Sam_Turbo/openerp-foundry/openerp-server/openerp/addons/kg_purchase_order/images/Verified.jpg'
 				if os.path.isfile(myfile) == True:
 					os.remove(myfile)
@@ -91,7 +68,7 @@ class onscreen_purchase_order_report(JasperDataParser.JasperDataParser):
 				pass
 		elif rec.state == 'approved':
 			if rec.confirmed_by.signature:
-				confirmed_signature = rec.confirmed_by.signature
+				confirmed_signature = rec.confirmed_by.scaned_signature
 				myfile = '/OpenERP/Sam_Turbo/openerp-foundry/openerp-server/openerp/addons/kg_purchase_order/images/Confirmed.jpg'
 				if os.path.isfile(myfile) == True:
 					os.remove(myfile)
@@ -103,7 +80,7 @@ class onscreen_purchase_order_report(JasperDataParser.JasperDataParser):
 			else:
 				pass
 			if rec.verified_by.signature and rec.approval_flag == True:
-				verified_signature = rec.verified_by.signature
+				verified_signature = rec.verified_by.scaned_signature
 				myfile = '/OpenERP/Sam_Turbo/openerp-foundry/openerp-server/openerp/addons/kg_purchase_order/images/Verified.jpg'
 				if os.path.isfile(myfile) == True:
 					os.remove(myfile)
@@ -115,7 +92,7 @@ class onscreen_purchase_order_report(JasperDataParser.JasperDataParser):
 			else:
 				pass
 			if rec.approved_by.signature:
-				approved_signature = rec.approved_by.signature
+				approved_signature = rec.approved_by.scaned_signature
 				myfile = '/OpenERP/Sam_Turbo/openerp-foundry/openerp-server/openerp/addons/kg_purchase_order/images/MD.jpg'
 				if os.path.isfile(myfile) == True:
 					os.remove(myfile)
@@ -128,7 +105,6 @@ class onscreen_purchase_order_report(JasperDataParser.JasperDataParser):
 				pass
 		
 		val['po_id'] = ids[0]
-		print "val....................",val
 		
 		return val
 	
