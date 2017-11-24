459ae3f172841e46ba226134adb8119ce4f10801
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2017-11-24 11:44:48 +0530

    cron for sch approval done by sujith

---
+++ schedulers/scheduler_sch_app_mail.py
@@ -0,0 +1,16 @@
+import xmlrpclib
+username = 'admin' #the user
+pwd = 'admin'      #the password of the user
+dbname = 'sam_phaseI'    #the database
+# Server Connectivity
+
+sock_common = xmlrpclib.ServerProxy ('http://localhost:8069/xmlrpc/common')
+
+uid = sock_common.login(dbname, username, pwd)
+
+sock = xmlrpclib.ServerProxy('http://localhost:8069/xmlrpc/object')
+
+## Scheduler List#
+
+sock.execute(dbname, uid, pwd, 'kg.mail.queue', 'send_mail')
+
