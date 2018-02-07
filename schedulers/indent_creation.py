94772642e298fa0657534e6a2da1b0e673ec6451
sengottuvelu <sengottuvelu@sengottuvelu.(none)>
2018-02-07 15:22:48 +0530

    Scheduler Changes changes done by Karthikeyan

---
+++ schedulers/indent_creation.py
@@ -0,0 +1,17 @@
+import xmlrpclib
+username = 'admin' #the user
+pwd = 'admin@123'      #the password of the user
+dbname = 'sam_phaseI'    #the database
+# Server Connectivity
+
+sock_common = xmlrpclib.ServerProxy ('http://localhost:8069/xmlrpc/common')
+
+uid = sock_common.login(dbname, username, pwd)
+
+sock = xmlrpclib.ServerProxy('http://localhost:8069/xmlrpc/object')
+
+## Scheduler List#
+
+
+sock.execute(dbname, uid, pwd, 'kg.scheduler', 'create_sch_indent')
+
